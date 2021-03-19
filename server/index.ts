ccb71246426db749f16b422b43387f3d931e3195
Unknown <paoolkim@gmail.com>
2021-03-18 18:04:09 -0700

    initial commit

---
+++ server/index.ts
@@ -0,0 +1,30 @@
+import next, { NextApiHandler } from 'next';
+import express, { Express, Request, Response } from 'express';
+import * as http from 'http';
+import helmet from 'helmet';
+import cors from 'cors';
+
+// import router from './routes';
+
+const port: number = parseInt(process.env.PORT || '3333', 10);
+const dev: boolean = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const nextHandler: NextApiHandler = nextApp.getRequestHandler();
+
+nextApp.prepare().then(async () => {
+  const app: Express = express();
+  const server: http.Server = http.createServer(app);
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.json());
+
+  app.get('/hello', async (_: Request, res: Response) => {
+    res.send('Hello World');
+  });
+  // app.use('/', router.router);
+  app.all('*', (req: any, res: any) => nextHandler(req, res));
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
