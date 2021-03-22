9ccfe19553f65560728f427e99fc3306b5504d50
Unknown <paoolkim@gmail.com>
2021-03-22 15:12:06 -0700

    app router

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import * as http from 'http';
 import helmet from 'helmet';
 import cors from 'cors';
 
-// import router from './routes';
+import router from './routes';
 
 const port: number = parseInt(process.env.PORT || '3333', 10);
 const dev: boolean = process.env.NODE_ENV !== 'production';
@@ -21,7 +21,7 @@ nextApp.prepare().then(async () => {
   app.get('/hello', async (_: Request, res: Response) => {
     res.send('Hello World');
   });
-  // app.use('/', router.router);
+  app.use('/', router);
   app.all('*', (req: any, res: any) => nextHandler(req, res));
 
   server.listen(port, () => {
