67c3c6422a9c4399387014e8cb7f462a19ab2440
Unknown <paoolkim@gmail.com>
2021-03-26 20:39:49 -0700

    add transaction details endpoint

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import addressRouter from './address';
+import transactionRouter from './transactions';
 
 const router = Router();
 
 router.use('/address', addressRouter);
+router.use('/transactions', transactionRouter);
 
 export default router;
