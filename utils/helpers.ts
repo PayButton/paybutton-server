241b3b43e34e3e11e24339691090a427cf309436
Andre Cabrera <contato@andrecabrera.com>
2022-03-24 06:08:42 -0300

    ci: fix for branch variable not being passed to front

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,8 +1,7 @@
 const port = process.env.APP_PORT || 3000
 
 export const getAppUrl = () : string => {
-    const CURRENT_BRANCH = process.env.BRANCH.replace('/','-') || 'master'
-    console.log(CURRENT_BRANCH)
+    const CURRENT_BRANCH = process.env.BRANCH.replace('/','-')
     const APP_BASE_URL = process.env.APP_BASE_URL || 'localhost'
     const APP_URL = CURRENT_BRANCH !== 'master' ? `https://${CURRENT_BRANCH}.${APP_BASE_URL}` : `https://${APP_BASE_URL}`
     const ENV_APP_URL = process.env.NODE_ENV == 'production' ? APP_URL : `http://localhost:${port}`
