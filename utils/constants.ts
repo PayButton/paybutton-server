4fc1bf1024f3f4fe9d11758720476f856c07a747
Andre Cabrera <contato@andrecabrera.com>
2022-03-24 06:22:57 -0300

    ci: another fix for frontend config

---
+++ utils/constants.ts
@@ -0,0 +1,2 @@
+export const APP_BASE_URL = process.env.APP_BASE_URL
+export const BRANCH = process.env.BRANCH
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,9 +1,11 @@
+import { BRANCH, APP_BASE_URL } from './constants'
 const port = process.env.APP_PORT || 3000
 
+
 export const getAppUrl = () : string => {
-    const CURRENT_BRANCH = process.env.BRANCH.replace('/','-')
-    const APP_BASE_URL = process.env.APP_BASE_URL || 'localhost'
-    const APP_URL = CURRENT_BRANCH !== 'master' ? `https://${CURRENT_BRANCH}.${APP_BASE_URL}` : `https://${APP_BASE_URL}`
+    const CURRENT_BRANCH = BRANCH || 'master'
+    const FORMATTED_BRANCH = CURRENT_BRANCH.replace('/', '-')
+    const APP_URL = CURRENT_BRANCH !== 'master' ? `https://${FORMATTED_BRANCH}.${APP_BASE_URL}` : `https://${APP_BASE_URL}`
     const ENV_APP_URL = process.env.NODE_ENV == 'production' ? APP_URL : `http://localhost:${port}`
     return ENV_APP_URL
 }
