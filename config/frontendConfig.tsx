322ce85a34c81aea2eccb096eb49d1d553610a54
Andre Cabrera <contato@andrecabrera.com>
2022-03-24 04:27:05 -0300

    refactor: refactor js to ts/tsx accordingly

---
+++ config/frontendConfig.tsx
@@ -0,0 +1,21 @@
+import ThirdPartyEmailPasswordReact from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
+import SessionReact from 'supertokens-auth-react/recipe/session'
+import { appInfo } from './appInfo'
+
+export let frontendConfig = () => {
+  return {
+    appInfo,
+    recipeList: [
+      ThirdPartyEmailPasswordReact.init({
+        emailVerificationFeature: {
+          mode: 'REQUIRED',
+        },
+        signInAndUpFeature: {
+          providers: [
+          ],
+        },
+      }),
+      SessionReact.init(),
+    ],
+  }
+}
