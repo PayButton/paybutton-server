7ad7dae3bfdcfaae1bc1de75ec99c5ef8a577651
Andre Cabrera <contato@andrecabrera.com>
2022-03-24 05:35:16 -0300

    refactor: fix typo

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,7 +1,7 @@
 const port = process.env.APP_PORT || 3000
 
 export const getAppUrl = () : string => {
-    const CURRENT_BRANCH = process.env.APP_BRANCH || 'master'
+    const CURRENT_BRANCH = process.env.BRANCH || 'master'
     console.log(CURRENT_BRANCH)
     const APP_BASE_URL = process.env.APP_BASE_URL || 'localhost'
     const APP_URL = CURRENT_BRANCH !== 'master' ? `https://${CURRENT_BRANCH}.${APP_BASE_URL}` : `https://${APP_BASE_URL}`
