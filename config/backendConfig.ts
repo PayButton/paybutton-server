ca3a3f6b8b530b7b982b3a966b2878f065fdb9f0
André Cabrera <andre@andrecabrera.com>
2022-04-28 23:26:10 -0300

    fix: run app with available social login credentials

--- config/backendConfig.ts
+++ config/backendConfig.ts
@@ -2,6 +2,44 @@ import ThirdPartyEmailPasswordNode from 'supertokens-node/recipe/thirdpartyemail
 import SessionNode from 'supertokens-node/recipe/session'
 import { appInfo } from './appInfo'
 import { TypeInput } from 'supertokens-node/types'
+
+const getSocialLoginProviders = () => {
+  const availableSocialProviders = {
+   'github': process.env.GITHUB_CLIENT_ID ? () => 
+          ThirdPartyEmailPasswordNode.Github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+          }) : false,
+   'google': process.env.GOOGLE_CLIENT_ID ? () =>
+          ThirdPartyEmailPasswordNode.Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          }) : false,
+   'facebook': process.env.FACEBOOK_CLIENT_ID ? () => 
+          ThirdPartyEmailPasswordNode.Facebook({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+          }) : false,
+   'apple': process.env.APPLE_CLIENT_ID ? () => 
+          ThirdPartyEmailPasswordNode.Apple({
+            clientId: process.env.APPLE_CLIENT_ID,
+            clientSecret: {
+                keyId: process.env.APPLE_KEY_ID,
+                privateKey: process.env.APPLE_PRIVATE_KEY,
+                teamId: process.env.APPLE_TEAM_ID
+            },
+          }) : false,
+  }
+
+  const socialProviderNodes = Object.keys(availableSocialProviders).map(providerKey => {
+    const getSocialProviderNode = availableSocialProviders[providerKey]
+    if (getSocialProviderNode) {
+      return getSocialProviderNode()
+    }
+  })
+
+  return socialProviderNodes.filter(provider => provider !== undefined)
+}
 export let backendConfig = () : TypeInput => {
   return {
     framework: 'express',
@@ -12,28 +50,7 @@ export let backendConfig = () : TypeInput => {
     appInfo,
     recipeList: [
       ThirdPartyEmailPasswordNode.init({
-        providers: [
-          ThirdPartyEmailPasswordNode.Github({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET
-          }),
-          ThirdPartyEmailPasswordNode.Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          }),
-          ThirdPartyEmailPasswordNode.Facebook({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-          }),
-          ThirdPartyEmailPasswordNode.Apple({
-            clientId: process.env.APPLE_CLIENT_ID,
-            clientSecret: {
-                keyId: process.env.APPLE_KEY_ID,
-                privateKey: process.env.APPLE_PRIVATE_KEY,
-                teamId: process.env.APPLE_TEAM_ID
-            },
-          }),
-        ],
+        providers: getSocialLoginProviders()
       }),
       SessionNode.init(),
     ],
