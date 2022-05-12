97738da12923d0301a0607b421c5f1eb0dd40d94
chedieck <estevao@lobo.dev.br>
2022-05-12 12:06:15 -0300

    fixed syntax to insert into table within transaction

--- db/services/paybuttonsService.ts
+++ db/services/paybuttonsService.ts
@@ -6,7 +6,7 @@ export async function createPaybutton (userId: string, prefixedAddressList: stri
     return await models.sequelize.transaction(async (t) => {
         let paybutton = await models.paybuttons.create({
             providerUserId: userId,
-        })
+        }, { transaction: t } )
 
         for (let i=0; i < prefixedAddressList.length; i++) {
             let addressWithPrefix = prefixedAddressList[i];
@@ -16,7 +16,7 @@ export async function createPaybutton (userId: string, prefixedAddressList: stri
                 address: address,
                 chainId:  chain.id,
                 paybuttonId: paybutton.id
-            });
+            }, { transaction: t } );
         }
         return paybutton
     });
