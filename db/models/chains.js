847468f98a8d44afdfb6b829320912a0828406da
chedieck <estevao@lobo.dev.br>
2022-05-12 12:09:15 -0300

    updated model files

--- db/models/chains.js
+++ db/models/chains.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-        chains.addresses = chains.hasMany(models.paybuttons_addresses, { as: 'addresses' });
+        chains.addresses = chains.hasMany(models.paybutton_addresses, { as: 'addresses' });
     }
   }
   chains.init({
