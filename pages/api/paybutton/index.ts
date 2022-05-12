635a8d0030e7b0eda1a88d24994758aeef9f92d3
chedieck <estevao@lobo.dev.br>
2022-05-12 12:06:01 -0300

    trimming addresses list

--- pages/api/paybutton/index.ts
+++ pages/api/paybutton/index.ts
@@ -18,7 +18,7 @@ const fetchResource = (userIdFromQuery: string): PayButton[] => {
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method == 'POST') {
-        const prefixedAddressList = req.body.addresses.split('\n')
+        const prefixedAddressList = req.body.addresses.trim().split('\n')
         paybuttonsService.createPaybutton('mocked-user-id', prefixedAddressList).then(
             function (paybutton) {
                 res.status(200).json(paybutton);
