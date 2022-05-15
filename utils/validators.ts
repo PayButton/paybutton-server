dfb7c7eb275d86ad046f70b14a4bb56f09c8f82f
chedieck <estevao@lobo.dev.br>
2022-05-15 17:06:59 -0300

    using validator on backend before adding paybutton addresses to database

--- utils/validators.ts
+++ utils/validators.ts
@@ -1,24 +1,21 @@
 import { supportedChains, supportedAddressesPattern } from 'types/constants'
 
-export const validateInput = function (value: string): boolean {
+export const validateAddresses = function (prefixedAddressList: string[]): boolean {
   /**
    * Disallow addresses without a 'chain:' prefix
    * Disallow addresses with an unknown prefix
    * Disallow  addresses is in an invalid format
-   * */
-  if (value) {
-    const addressList = value.trim().split('\n')
-    for (let i = 0; i < addressList.length; i++) {
-      const addressWithPrefix = addressList[i];
-      const [prefix, address] =  addressWithPrefix.split(':')
-      if(
-        !addressWithPrefix.includes(':')
-        || !supportedChains.includes(prefix)
-        || !address.match(supportedAddressesPattern)
-      ) {
-        return false
-      }
+   **/
+  for (let i = 0; i < prefixedAddressList.length; i++) {
+    const addressWithPrefix = prefixedAddressList[i];
+    const [prefix, address] =  addressWithPrefix.split(':')
+    if(
+      !addressWithPrefix.includes(':')
+      || !supportedChains.includes(prefix)
+      || !address.match(supportedAddressesPattern)
+    ) {
+      return false
     }
-    return true
   }
+  return true
 } // WIP unittests
