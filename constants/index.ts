5b978be921fda0cf705b59bbfe8b2736b8b553dc
chedieck <estevao@lobo.dev.br>
2022-05-17 20:47:16 -0300

    added constants for response messages

--- constants/index.ts
+++ constants/index.ts
@@ -3,3 +3,9 @@ export const SUPPORTED_CHAINS = [
   'bitcoincash'
 ] // WIP integration tests to check if this corresponds to the database
 export const SUPPORTED_ADDRESS_PATTERN = /(q|p)[a-z0-9]{41}/
+
+export const REPONSE_MESSAGES = {
+  INVALID_INPUT_400: { statusCode: 400, message: 'Invalid input.' },
+  USER_ID_NOT_PROVIDED_400: { statusCode: 400, message: "'userId' not provided."},
+  NOT_FOUND_404: { statusCode: 404, message: "Not found."}
+}
