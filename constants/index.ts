6d31a0448929d4d638d17283b463b137f95a662a
chedieck <estevao@lobo.dev.br>
2022-05-17 14:39:30 -0300

    moved constants file to dedicated folder

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 export const supportedChains = [
   'ecash',
   'bitcoincash'
-] // WIP unittests to check if this corresponds to the database
+] // WIP integration tests to check if this corresponds to the database
 export const supportedAddressesPattern = /(q|p)[a-z0-9]{41}/
