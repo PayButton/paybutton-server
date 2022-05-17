45a22b9a359f3594151d70f4c62e54e9201aa1d5
chedieck <estevao@lobo.dev.br>
2022-05-17 19:35:31 -0300

    added type to Chain services

--- services/chainsService.ts
+++ services/chainsService.ts
@@ -1,5 +1,10 @@
 import models from 'db/models/index'
+import { Chain } from 'types'
 
-export async function getChainFromSlug (slug: string) {
+export async function getChainFromSlug(slug: string): Promise<Chain> {
     return models.chains.findOne( { where: { slug: slug } })
+}
+
+export async function getAllChainSlugs (): Promise<Chain[]> {
+    return models.chains.findAll( { attributes: [ 'slug' ] })
 }
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,14 @@
+// By default, Sequelize automatically adds the fields createdAt and updatedAt to every model,
+// so they are also present in these types
+
 export type PayButton = {
         id: string,
         userId: string,
         addresses: any,
+}
+
+export type Chain = {
+        id: string,
+        slug: string,
+        title: string,
 }
