3273769c105c709e37c44d45de628ae404377094
chedieck <estevao@lobo.dev.br>
2022-05-18 20:52:51 -0300

    improved address pattern restrictivity

--- constants/index.ts
+++ constants/index.ts
@@ -2,4 +2,4 @@ export const supportedChains = [
   'ecash',
   'bitcoincash'
 ] // WIP integration tests to check if this corresponds to the database
-export const supportedAddressesPattern = /(q|p)[a-z0-9]{41}/
+export const SUPPORTED_ADDRESS_PATTERN = /((q|p)[qpzry9x8gf2tvdw0s3jn54khce6mua7l]{41}|(Q|P)[QPZRY9X8GF2TVDW0S3JN54KHCE6MUA7L]{41})/
