759fa2f2b2de812f9c2f32fe2525a97b32617553
chedieck <estevao@lobo.dev.br>
2022-05-19 09:59:43 -0300

    added test chains to database and constant

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,7 @@
 export const supportedChains = [
   'ecash',
-  'bitcoincash'
+  'bitcoincash',
+  'bchtest',
+  'bchreg'
 ] // WIP integration tests to check if this corresponds to the database
 export const SUPPORTED_ADDRESS_PATTERN = /((q|p)[qpzry9x8gf2tvdw0s3jn54khce6mua7l]{41}|(Q|P)[QPZRY9X8GF2TVDW0S3JN54KHCE6MUA7L]{41})/
