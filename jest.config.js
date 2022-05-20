ae612a548a30dc0ef5ac67267e1fdba74d4e9752
chedieck <estevao@lobo.dev.br>
2022-05-20 14:23:17 -0300

    added `mysql-test` package

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,6 @@ module.exports = {
   testEnvironment: 'node',
   modulePaths: ['/app/src/'],
   setupFilesAfterEnv: ["/app/src/tests/setup.js"]
+  globalSetup: "/app/src/node_modules/@databases/mysql-test/jest/globalSetup",
+  globalTeardown: "/app/src/node_modules/@databases/mysql-test/jest/globalTeardown",
 };
