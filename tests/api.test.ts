5bb4c01986915a63501228f7c0559597238e6032
chedieck <estevao@lobo.dev.br>
2022-05-24 11:53:15 -0300

    created integration tests for post end-point

---
+++ tests/api.test.ts
@@ -0,0 +1,79 @@
+// import * as paybuttonsService from 'services/paybuttonsService'
+import models from 'db/models/index'
+import paybuttonEndpoint from 'pages/api/paybutton/index';
+const httpMocks = require('node-mocks-http');
+
+
+describe('POST /api/paybutton/', () => {
+  let baseRequestJSON = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: {
+      userId: 'test-u-id',
+      addresses:'ecash:qpz274aaj98xxnnkus8hzv367za28j900c7tv5v8pc\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
+    }
+  };
+
+  it('should succeed', () => {
+    const req  = httpMocks.createRequest(baseRequestJSON);
+    const res = httpMocks.createResponse();
+    paybuttonEndpoint(req, res).then(() => {
+      const resposeData = res._getJSONData();
+      expect(res.statusCode).toBe(200)
+      expect(resposeData.providerUserId).toBe('test-u-id')
+      expect(resposeData.addresses).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            address: 'qpz274aaj98xxnnkus8hzv367za28j900c7tv5v8pc'
+          }),
+          expect.objectContaining({
+            address: 'qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
+          }),
+        ])
+      )
+    })
+  });
+
+  it('should fail without userId', async () => {
+    baseRequestJSON.body = { 
+      userId: undefined,
+      addresses:'ecash:qpz274aaj98xxnnkus8hzv367za28j900c7tv5v8pc\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
+    }
+    const req = httpMocks.createRequest(baseRequestJSON)
+    const res = httpMocks.createResponse();
+    await paybuttonEndpoint(req, res)
+    expect(res.statusCode).toBe(400)
+  });
+
+  it('should fail without addresses', async () => {
+    baseRequestJSON.body = { 
+      userId: 'test-u-id',
+      addresses: undefined,
+    }
+    const req = httpMocks.createRequest(baseRequestJSON)
+    const res = httpMocks.createResponse();
+    await paybuttonEndpoint(req, res)
+    expect(res.statusCode).toBe(400)
+  });
+
+  it('should fail with invalid addresses', async () => {
+    baseRequestJSON.body = { 
+      userId: 'test-u-id',
+      addresses:'ecash:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
+    }
+    const req = httpMocks.createRequest(baseRequestJSON)
+    const res = httpMocks.createResponse();
+    await paybuttonEndpoint(req, res)
+    expect(res.statusCode).toBe(400)
+  });
+
+  /*
+  it('should have created only one paybutton with two addresses', async () => {
+    const pbs = await models.paybuttons.findAll()
+    console.log(pbs)
+  })
+  */
+
+})
