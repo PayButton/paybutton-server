e34bd084c34eb28bba9c6aae28be17bfef2116d1
chedieck <estevao@lobo.dev.br>
2022-05-26 09:01:13 -0300

    removed comments

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,3 @@
-// By default, Sequelize automatically adds the fields createdAt and updatedAt to every model,
-// so they are also present in these types
-
 export type PayButton = {
         id: string,
         userId: string,
