6380302050e427d6693068f95d9d184025c7a21a
chedieck <estevao@lobo.dev.br>
2022-05-30 09:44:50 -0300

    refactored api endpoint, validation & unittests to user throw/catch paradigm

--- pages/api/paybutton/index.ts
+++ pages/api/paybutton/index.ts
@@ -7,32 +7,36 @@ import { RESPONSE_MESSAGES } from 'constants/index'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const values = req.body
   const userId = values.userId
-  if (!userId) {
-    res.status(400).json(RESPONSE_MESSAGES.USER_ID_NOT_PROVIDED_400)
-    return
-  }
   if (req.method == 'POST') {
-    const prefixedAddressList = values.addresses.trim().split('\n')
-    if (validateAddresses(prefixedAddressList)) {
-      try {
-        const paybutton = await paybuttonsService.createPaybutton(userId, prefixedAddressList)
-        res.status(200).json(paybutton);
-      }
-      catch (err) {
-        res.status(500).json({ statusCode: 500, message: err.message })
-      }
+    const addresses = values.addresses
+    const prefixedAddressList = addresses.trim().split('\n')
+    try {
+      if (!userId) throw new Error(RESPONSE_MESSAGES.USER_ID_NOT_PROVIDED_400.message)
+      validateAddresses(prefixedAddressList)
+      const paybutton = await paybuttonsService.createPaybutton(userId, prefixedAddressList)
+      res.status(200).json(paybutton);
     }
-    else {
-        res.status(400).json(RESPONSE_MESSAGES.INVALID_INPUT_400)
+    catch (err: any) {
+      switch (err.message) 
+      {
+        case RESPONSE_MESSAGES.INVALID_INPUT_400.message:
+          res.status(400).json(RESPONSE_MESSAGES.INVALID_INPUT_400)
+        case RESPONSE_MESSAGES.ADDRESSES_NOT_PROVIDED_400.message:
+          res.status(400).json(RESPONSE_MESSAGES.ADDRESSES_NOT_PROVIDED_400)
+        case RESPONSE_MESSAGES.USER_ID_NOT_PROVIDED_400.message:
+          res.status(400).json(RESPONSE_MESSAGES.USER_ID_NOT_PROVIDED_400)
+        default:
+          res.status(500).json({ statusCode: 500, message: err.message })
+      }
     }
   }
   else if (req.method == 'GET') {
     try {
       const paybuttonList = await paybuttonsService.fetchPaybuttonArrayByUserId(userId)
       res.status(200).json(paybuttonList);
-     }
+    }
     catch (err) {
-       res.status(500).json({ statusCode: 500, message: err.message })
-     }
+      res.status(500).json({ statusCode: 500, message: err.message })
+    }
   }
 }
--- utils/validators.ts
+++ utils/validators.ts
@@ -1,6 +1,7 @@
 import { SUPPORTED_CHAINS, SUPPORTED_ADDRESS_PATTERN } from 'constants/index'
+import { RESPONSE_MESSAGES } from 'constants/index'
 
-export const validateAddresses = function (prefixedAddressList: string[]): boolean {
+export const validateAddresses = function (prefixedAddressList: string[]): void {
   /**
    * Disallow addresses without a 'chain:' prefix
    * Disallow addresses with an unknown prefix
@@ -8,6 +9,7 @@ export const validateAddresses = function (prefixedAddressList: string[]): boole
    * Disallow  addresses with repeated prefixes (chains)
    **/
   let seenPrefixes = new Set()
+  if (!prefixedAddressList.length) throw new Error(RESPONSE_MESSAGES.ADDRESSES_NOT_PROVIDED_400.message);
   for (let i = 0; i < prefixedAddressList.length; i++) {
     const addressWithPrefix = prefixedAddressList[i];
     let [prefix, address] =  addressWithPrefix.split(':')
@@ -18,9 +20,8 @@ export const validateAddresses = function (prefixedAddressList: string[]): boole
       || !address.match(SUPPORTED_ADDRESS_PATTERN)
       || seenPrefixes.has(prefix)
     ) {
-      return false
+      throw new Error(RESPONSE_MESSAGES.INVALID_INPUT_400.message)
     }
     seenPrefixes.add(prefix)
   }
-  return prefixedAddressList.length ? true : false
 }
