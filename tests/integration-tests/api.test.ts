2d413976d17990176a4328ac77ba3567015179fb
chedieck <estevao@lobo.dev.br>
2022-05-31 09:21:29 -0300

    reorganized tests paths

--- tests/integration-tests/api.test.ts
+++ tests/integration-tests/api.test.ts
@@ -1,28 +1,27 @@
 import models from 'db/models/index'
-import paybuttonEndpoint from 'pages/api/paybutton/index';
-import { testEndpoint, clearPaybuttons, clearPaybuttonAddresses } from './utils'
-
+import paybuttonEndpoint from 'pages/api/paybutton/index'
+import { testEndpoint, clearPaybuttons, clearPaybuttonAddresses } from 'tests/utils'
 
 afterAll(() => {
-  clearPaybuttons()
-  clearPaybuttonAddresses()
-});
+  void clearPaybuttons()
+  void clearPaybuttonAddresses()
+})
 
 describe('POST /api/paybutton/', () => {
-  let baseRequestJSON = {
+  const baseRequestJSON = {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: {
       userId: 'test-u-id',
-      addresses:'ecash:qpz274aaj98xxnnkus8hzv367za28j900c7tv5v8pc\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
+      addresses: 'ecash:qpz274aaj98xxnnkus8hzv367za28j900c7tv5v8pc\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
     }
-  };
+  }
 
   it('should succeed', async () => {
     const res = await testEndpoint(baseRequestJSON, paybuttonEndpoint)
-    const resposeData = res._getJSONData();
+    const resposeData = res._getJSONData()
     expect(res.statusCode).toBe(200)
     expect(resposeData.providerUserId).toBe('test-u-id')
     expect(resposeData.addresses).toEqual(
@@ -32,37 +31,37 @@ describe('POST /api/paybutton/', () => {
         }),
         expect.objectContaining({
           address: 'qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
-        }),
+        })
       ])
     )
-  });
+  })
 
   it('should fail without userId', async () => {
-    baseRequestJSON.body = { 
+    baseRequestJSON.body = {
       userId: undefined,
-      addresses:'ecash:qpz274aaj98xxnnkus8hzv367za28j900c7tv5v8pc\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
+      addresses: 'ecash:qpz274aaj98xxnnkus8hzv367za28j900c7tv5v8pc\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
     }
     const res = await testEndpoint(baseRequestJSON, paybuttonEndpoint)
     expect(res.statusCode).toBe(400)
-  });
+  })
 
   it('should fail without addresses', async () => {
-    baseRequestJSON.body = { 
+    baseRequestJSON.body = {
       userId: 'test-u-id',
-      addresses: undefined,
+      addresses: undefined
     }
     const res = await testEndpoint(baseRequestJSON, paybuttonEndpoint)
     expect(res.statusCode).toBe(400)
-  });
+  })
 
   it('should fail with invalid addresses', async () => {
-    baseRequestJSON.body = { 
+    baseRequestJSON.body = {
       userId: 'test-u-id',
-      addresses:'ecash:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
+      addresses: 'ecash:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
     }
     const res = await testEndpoint(baseRequestJSON, paybuttonEndpoint)
     expect(res.statusCode).toBe(400)
-  });
+  })
 
   describe('Test database update', () => {
     it('should have created only one paybutton with two addresses', async () => {
@@ -71,5 +70,5 @@ describe('POST /api/paybutton/', () => {
       expect(paybuttonList.length).toBe(1)
       expect(paybuttonAddressList.length).toBe(2)
     })
-  });
-});
+  })
+})
