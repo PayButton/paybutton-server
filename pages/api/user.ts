40010ac2f7d8a4220372ff1ec992330100a58a6c
André Cabrera <git@andrecabrera.com>
2022-06-05 17:19:07 -0300

    chore: fix lint for user.ts

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,11 +1,11 @@
 import { superTokensNextWrapper } from 'supertokens-node/nextjs'
 import { verifySession } from 'supertokens-node/recipe/session/framework/express'
-import supertokens from 'supertokens-node'
+import { SessionRequest } from 'supertokens-node/framework/express'; import supertokens from 'supertokens-node'
 import * as SuperTokensConfig from '../../config/backendConfig'
 
 supertokens.init(SuperTokensConfig.backendConfig())
 
-export default async function user(req, res) {
+export default async function user (req: SessionRequest, res: any): Promise<any> {
   await superTokensNextWrapper(
     async (next) => {
       return await verifySession()(req, res, next)
@@ -18,6 +18,6 @@ export default async function user(req, res) {
     note: 'Fetch any data from your application for authenticated user after using verifySession middleware',
     userId: req.session.getUserId(),
     sessionHandle: req.session.getHandle(),
-    accessTokenPayload: req.session.getAccessTokenPayload(),
+    accessTokenPayload: req.session.getAccessTokenPayload()
   })
 }
