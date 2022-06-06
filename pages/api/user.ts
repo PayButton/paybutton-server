415039749348919c69fa2c96a62f111b695a47af
André Cabrera <git@andrecabrera.com>
2022-06-06 20:20:02 -0300

    fix: remove any type

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,11 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Response } from 'express'
 import { superTokensNextWrapper } from 'supertokens-node/nextjs'
 import { verifySession } from 'supertokens-node/recipe/session/framework/express'
-import { SessionRequest } from 'supertokens-node/framework/express'; import supertokens from 'supertokens-node'
+import { SessionRequest } from 'supertokens-node/framework/express'
+import supertokens from 'supertokens-node'
 import * as SuperTokensConfig from '../../config/backendConfig'
 
 supertokens.init(SuperTokensConfig.backendConfig())
 
-export default async function user (req: SessionRequest, res: any): Promise<any> {
+export default async function user (req: NextApiRequest & SessionRequest, res: NextApiResponse & Response): Promise<any> {
   await superTokensNextWrapper(
     async (next) => {
       return await verifySession()(req, res, next)
