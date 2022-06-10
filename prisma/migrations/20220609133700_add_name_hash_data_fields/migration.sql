ca0a3883b58ccf9154d2fcd7a173b72f7d659a4c
chedieck <estevao@lobo.dev.br>
2022-06-10 09:08:41 -0300

    changed: buttonData field not nullable (null value should be saved as '{}')

--- prisma/migrations/20220609133700_add_name_hash_data_fields/migration.sql
+++ prisma/migrations/20220609133700_add_name_hash_data_fields/migration.sql
@@ -2,9 +2,9 @@ ALTER TABLE `Paybutton`
 ADD COLUMN `name` VARCHAR(255) NOT NULL AFTER `id`;
 
 ALTER TABLE `Paybutton`
-ADD COLUMN `uuid` UUID AFTER `name`;
+ADD COLUMN `uuid` UUID AFTER `name`; -- Prisma does not support this type, so it must be nullable.
 
 ALTER TABLE `Paybutton`
-ADD COLUMN `buttonData` JSON AFTER `uuid`;
+ADD COLUMN `buttonData` JSON NOT NULL AFTER `uuid`;
 
 ALTER TABLE `Paybutton` ADD CONSTRAINT `Paybutton_name_providerUserId_unique_constraint` UNIQUE (`name`, `providerUserId`);
