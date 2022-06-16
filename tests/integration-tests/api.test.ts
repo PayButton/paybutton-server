af2a44dc5a1635a3146540a57b26dc206e046c73
chedieck <estevao@lobo.dev.br>
2022-06-16 11:23:52 -0300

    removed redundant "Should"

--- tests/integration-tests/api.test.ts
+++ tests/integration-tests/api.test.ts
@@ -29,7 +29,7 @@ describe('POST /api/paybutton/', () => {
     }
   }
 
-  it('Should create a paybutton with two addresses', async () => {
+  it('Create a paybutton with two addresses', async () => {
     const res = await testEndpoint(baseRequestOptions, paybuttonEndpoint)
     const responseData = res._getJSONData()
     expect(res.statusCode).toBe(200)
@@ -48,7 +48,7 @@ describe('POST /api/paybutton/', () => {
     void expect(countPaybuttonAddresses()).resolves.toBe(2)
   })
 
-  it('Should fail without userId', async () => {
+  it('Fail without userId', async () => {
     baseRequestOptions.body = {
       userId: '',
       addresses: 'ecash:qpz274aaj98xxnnkus8hzv367za28j900c7tv5v8pc\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
@@ -59,7 +59,7 @@ describe('POST /api/paybutton/', () => {
     expect(responseData.message).toBe(RESPONSE_MESSAGES.USER_ID_NOT_PROVIDED_400.message)
   })
 
-  it('Should fail without addresses', async () => {
+  it('Fail without addresses', async () => {
     baseRequestOptions.body = {
       userId: 'test-u-id',
       addresses: ''
@@ -70,7 +70,7 @@ describe('POST /api/paybutton/', () => {
     expect(responseData.message).toBe(RESPONSE_MESSAGES.ADDRESSES_NOT_PROVIDED_400.message)
   })
 
-  it('Should fail with invalid addresses', async () => {
+  it('Fail with invalid addresses', async () => {
     baseRequestOptions.body = {
       userId: 'test-u-id',
       addresses: 'ecash:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbitcoincash:qz0dqjf6w6dp0lcs8cc68s720q9dv5zv8cs8fc0lt4'
@@ -104,7 +104,7 @@ describe('GET /api/paybutton/', () => {
     }
   }
 
-  it('Should get 3 paybuttons for userA', async () => {
+  it('Get 3 paybuttons for userA', async () => {
     const res = await testEndpoint(baseRequestOptions, paybuttonEndpoint)
     expect(res.statusCode).toBe(200)
     const responseData = res._getJSONData()
@@ -112,7 +112,7 @@ describe('GET /api/paybutton/', () => {
     expect(responseData.length).toBe(3)
   })
 
-  it('Should get 1 paybuttons for userB', async () => {
+  it('Get 1 paybuttons for userB', async () => {
     baseRequestOptions.body = {
       userId: userB
     }
@@ -134,7 +134,7 @@ describe('GET /api/paybutton/', () => {
     expect(responseData[0]).toHaveProperty('providerUserId')
   })
 
-  it('Should get no paybuttons for unknown user', async () => {
+  it('Get no paybuttons for unknown user', async () => {
     baseRequestOptions.body = {
       userId: 'unknown-user'
     }
@@ -144,7 +144,7 @@ describe('GET /api/paybutton/', () => {
     expect(responseData.length).toBe(0)
   })
 
-  it('Should fail without userId', async () => {
+  it('Fail without userId', async () => {
     baseRequestOptions.body = {
       userId: ''
     }
@@ -178,7 +178,7 @@ describe('GET /api/paybutton/[id]', () => {
     query: {}
   }
 
-  it('Should find paybutton for created ids', async () => {
+  it('Find paybutton for created ids', async () => {
     for (const id of createdPaybuttonsIds) {
       if (baseRequestOptions.query != null) baseRequestOptions.query.id = id
       const res = await testEndpoint(baseRequestOptions, paybuttonIdEndpoint)
@@ -198,7 +198,7 @@ describe('GET /api/paybutton/[id]', () => {
     }
   })
 
-  it('Should not find paybutton for next id', async () => {
+  it('Not find paybutton for next id', async () => {
     const nextId = createdPaybuttonsIds[createdPaybuttonsIds.length - 1] + 1
     if (baseRequestOptions.query != null) baseRequestOptions.query.id = nextId
     const res = await testEndpoint(baseRequestOptions, paybuttonIdEndpoint)
