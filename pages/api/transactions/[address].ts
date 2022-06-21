fc363bda464817e147b823e60ebfd11eae8df502
git@andrecabrera.com <git@andrecabrera.com>
2022-06-21 20:30:16 -0300

    fix: wrap endpoint around if GET condition

--- pages/api/transactions/[address].ts
+++ pages/api/transactions/[address].ts
@@ -5,15 +5,18 @@ import { RESPONSE_MESSAGES } from 'constants/index'
 const { ADDRESS_NOT_PROVIDED_400 } = RESPONSE_MESSAGES
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  try {
-    const { address } = req.query
-    if (address === '' || address === undefined) {
-      res.status(ADDRESS_NOT_PROVIDED_400.statusCode).send({ message: ADDRESS_NOT_PROVIDED_400.message })
-    }
+  if (req.method === 'GET') {
+    try {
+      const { address } = req.query
 
-    const response = await getAddress(address)
-    res.status(200).send(response)
-  } catch (e) {
-    res.status(500).send(e.message)
+      if (address === '' || address === undefined) {
+        res.status(ADDRESS_NOT_PROVIDED_400.statusCode).send({ message: ADDRESS_NOT_PROVIDED_400.message })
+      }
+
+      const response = await getAddress(address)
+      res.status(200).send(response)
+    } catch (e) {
+      res.status(500).send(e.message)
+    }
   }
 }
