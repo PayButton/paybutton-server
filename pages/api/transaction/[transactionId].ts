7f7bc1d5a38a403f17d2afb5a8d2dc182099da57
git@andrecabrera.com <git@andrecabrera.com>
2022-06-23 13:11:36 -0300

    fix: refactor transaction api and fix tests

--- pages/api/transaction/[transactionId].ts
+++ pages/api/transaction/[transactionId].ts
@@ -3,16 +3,22 @@ import { getTransactionDetails } from 'services/bchdService'
 import { RESPONSE_MESSAGES } from 'constants/index'
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  try {
-    const { transactionId } = req.query
-
-    if (transactionId === '' || transactionId === undefined) {
-      res.status(400).send({ message: RESPONSE_MESSAGES.TRANSACTION_ID_NOT_PROVIDED_400.message })
+ if (req.method === 'GET') {
+    const transactionId = req.query.transactionId as string
+    try {
+      if (transactionId === '' || transactionId === undefined) {
+        throw new Error(RESPONSE_MESSAGES.TRANSACTION_ID_NOT_PROVIDED_400.message)
+      }
+      const response = await getTransactionDetails(transactionId)
+      res.status(200).send(response)
+    } catch (err: any) {
+      switch (err.message) {
+        case RESPONSE_MESSAGES.TRANSACTION_ID_NOT_PROVIDED_400.message:
+          res.status(RESPONSE_MESSAGES.TRANSACTION_ID_NOT_PROVIDED_400.statusCode).json(RESPONSE_MESSAGES.TRANSACTION_ID_NOT_PROVIDED_400)
+          break
+        default:
+          res.status(500).json({ statusCode: 500, message: err.message })
+      }
     }
-
-    const response = await getTransactionDetails(transactionId)
-    res.status(200).send(response)
-  } catch (e) {
-    res.status(500).send(e.message)
   }
 }
