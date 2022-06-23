68626af3301be88f32184420f25e09f5eda654bf
chedieck <estevao@lobo.dev.br>
2022-06-23 11:10:26 -0300

    added submit logic to form

--- components/Paybutton/PaybuttonForm.tsx
+++ components/Paybutton/PaybuttonForm.tsx
@@ -1,19 +1,30 @@
 import React, { ReactElement } from 'react'
+import { useForm } from 'react-hook-form'
+import { POSTParameters } from 'utils/validators'
 
-export default function PaybuttonForm (): ReactElement {
+interface IProps {
+  onSubmit: Function
+}
+
+export default function PaybuttonForm ({ onSubmit }: IProps): ReactElement {
+  const { register, handleSubmit } = useForm<POSTParameters>()
+
   return (
-    <form action='/api/paybutton' method='post'>
+    <form onSubmit={handleSubmit(onSubmit)} method='post'>
       <label htmlFor='name'>Name</label>
-      <input type='text' id='name' name='name' required />
+      <input {...register('name')} type='text' id='name' name='name' required />
+      <br />
 
       <label htmlFor='buttonData'>Button Data</label>
-      <textarea id='buttonData' name='buttonData' />
+      <textarea {...register('buttonData')} id='buttonData' name='buttonData' />
+      <br />
 
       <label htmlFor='addresses'>Addresses</label>
-      <textarea id='addresses' name='addresses' required />
+      <textarea {...register('addresses')} id='addresses' name='addresses' required />
+      <br />
 
       <label htmlFor='userId'>userId</label>
-      <textarea id='userId' name='userId' required />
+      <textarea {...register('userId')} id='userId' name='userId' required />
 
       <button type='submit'>Submit</button>
     </form>
