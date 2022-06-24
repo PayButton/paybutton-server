16acff35e9f34dcbb34a72617d765307f7d65c3a
chedieck <estevao@lobo.dev.br>
2022-06-24 12:30:51 -0300

    added prefix to specify sql statments run order

--- scripts/db/04-create-dev-user.sql
+++ scripts/db/04-create-dev-user.sql
@@ -1,3 +1,4 @@
+SELECT SLEEP(20);
 USE `supertokens`;
 INSERT INTO emailpassword_users VALUES ('dev-uid', 'dev@mail.com', '$2a$11$AGppTnFU3HBeHuH1z3kPb.A1WFUxHVVNmAmiMaHJ3QvVkWT58jxBO', 1652220489244);
 INSERT INTO emailverification_verified_emails VALUES ('dev-uid', 'dev@mail.com');
