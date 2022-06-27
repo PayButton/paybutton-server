7cc9531671db8c5fa531394f4e505675c01ee91e
chedieck <estevao@lobo.dev.br>
2022-06-27 15:46:54 -0300

    added permission to update supertokens for test db user

--- scripts/db/init-test-db.sql
+++ scripts/db/init-test-db.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE `paybutton-test`;
 CREATE USER 'paybutton-test'@localhost IDENTIFIED BY 'paybutton-test';
 GRANT ALL PRIVILEGES ON `paybutton-test`.* TO 'paybutton-test'@'%' IDENTIFIED BY 'paybutton-test' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON `supertokens`.* TO 'paybutton-test'@'%' IDENTIFIED BY 'paybutton-test' WITH GRANT OPTION;
