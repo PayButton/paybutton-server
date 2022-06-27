5fe6847d765c1e0cad36e796dbd5acd06e6e4893
chedieck <estevao@lobo.dev.br>
2022-06-27 15:24:00 -0300

    refactored form

--- components/Paybutton/PaybuttonForm.tsx
+++ components/Paybutton/PaybuttonForm.tsx
@@ -11,21 +11,22 @@ export default function PaybuttonForm ({ onSubmit }: IProps): ReactElement {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} method='post'>
-      <label htmlFor='name'>Name</label>
+      <label htmlFor='name'>Name</label> <br />
       <input {...register('name')} type='text' id='name' name='name' required />
       <br />
 
-      <label htmlFor='buttonData'>Button Data</label>
+      <label htmlFor='buttonData'>Button Data</label> <br />
       <textarea {...register('buttonData')} id='buttonData' name='buttonData' />
       <br />
 
-      <label htmlFor='addresses'>Addresses</label>
+      <label htmlFor='addresses'>Addresses</label> <br />
       <textarea {...register('addresses')} id='addresses' name='addresses' required />
       <br />
 
-      <label htmlFor='userId'>userId</label>
-      <textarea {...register('userId')} id='userId' name='userId' required />
+      <label htmlFor='userId'>userId</label> <br />
+      <input {...register('userId')} type='text' id='userId' name='userId' required />
 
+      <br />
       <button type='submit'>Submit</button>
     </form>
   )
