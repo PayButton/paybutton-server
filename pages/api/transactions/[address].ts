7863b9254a8e4c0e640c3c13e7e260a4e1fa3b60
chedieck <estevao@lobo.dev.br>
2022-07-14 15:12:10 -0300

    refactor: transaction endpoint fetched from the database

--- pages/api/transactions/[address].ts
+++ pages/api/transactions/[address].ts
@@ -1,18 +1,19 @@
 import { NextApiResponse, NextApiRequest } from 'next'
-import { getAddress } from 'services/bchdService'
+import { parseAddress } from 'utils/validators'
 import { RESPONSE_MESSAGES } from 'constants/index'
+import { fetchPaybuttonAddressBySubstring } from 'services/paybuttonAddressesService'
 
 const { ADDRESS_NOT_PROVIDED_400 } = RESPONSE_MESSAGES
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   if (req.method === 'GET') {
-    const address = req.query.address as string
     try {
+      const address = parseAddress(req.query.address as string)
       if (address === '' || address === undefined) {
         throw new Error(ADDRESS_NOT_PROVIDED_400.message)
       }
-      const response = await getAddress(address)
-      res.status(200).send(response)
+      const paybuttonAddress = await fetchPaybuttonAddressBySubstring(address)
+      res.status(200).send(paybuttonAddress.receivedTransactions)
     } catch (err: any) {
       switch (err.message) {
         case ADDRESS_NOT_PROVIDED_400.message:
