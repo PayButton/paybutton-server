51f64591e6e4e42268f0ed6e4186207546109008
chedieck <estevao@lobo.dev.br>
2022-07-21 15:09:28 -0300

    fix: ordering transactions by timestamp

--- pages/api/transactions/[address].ts
+++ pages/api/transactions/[address].ts
@@ -1,20 +1,20 @@
 import { NextApiResponse, NextApiRequest } from 'next'
 import { parseAddress } from 'utils/validators'
 import { RESPONSE_MESSAGES } from 'constants/index'
-import { fetchPaybuttonAddressBySubstring } from 'services/paybuttonAddressesService'
+import { fetchAddressTransactions } from 'services/transactionsService'
 import Cors from 'cors'
 
 const { ADDRESS_NOT_PROVIDED_400 } = RESPONSE_MESSAGES
 const cors = Cors({
-  methods: ['GET', 'HEAD'],
+  methods: ['GET', 'HEAD']
 })
 
-function runMiddleware(
+async function runMiddleware (
   req: NextApiRequest,
   res: NextApiResponse,
   fn: Function
-) {
-  return new Promise((resolve, reject) => {
+): Promise<any> {
+  return await new Promise((resolve, reject) => {
     fn(req, res, (result: any) => {
       if (result instanceof Error) {
         return reject(result)
@@ -25,16 +25,16 @@ function runMiddleware(
   })
 }
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    await runMiddleware(req, res, cors)
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  await runMiddleware(req, res, cors)
   if (req.method === 'GET') {
     try {
       if (req.query.address === '' || req.query.address === undefined) {
         throw new Error(ADDRESS_NOT_PROVIDED_400.message)
       }
       const address = parseAddress(req.query.address as string)
-      const paybuttonAddress = await fetchPaybuttonAddressBySubstring(address)
-      res.status(200).send(paybuttonAddress.receivedTransactions)
+      const transactions = await fetchAddressTransactions(address)
+      res.status(200).send(transactions)
     } catch (err: any) {
       switch (err.message) {
         case ADDRESS_NOT_PROVIDED_400.message:
