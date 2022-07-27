efa916766b8b1aa13d9444feb8485791ffc44864
John <jkuney17@gmail.com>
2022-07-27 17:35:31 -0500

    update chart

--- pages/dashboard/Chart.tsx
+++ pages/dashboard/Chart.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import type { NextPage } from "next"
+import React from "react"
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -8,8 +9,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+} from "chart.js"
+import { Line } from "react-chartjs-2"
 
 ChartJS.register(
   CategoryScale,
@@ -19,62 +20,68 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend
-);
+)
 
-export default function Chart ({data, usd}): React.ReactElement {
+interface Props {
+  data: object
+}
 
-    function cssvar(name) {
-        return getComputedStyle(document.body).getPropertyValue(name);
-      }
-    
-    const options = {
-        responsive: true,
-        lineTension: 0.4,
-        maintainAspectRatio: false,
-        plugins: {
-            legend: {
-            display: false,
-            },
-            tooltip: {
-            displayColors: false,
-            // callbacks: {
-            //     title: function () {
-            //       return "my tittle";
-            //     }
-            //   }
-            mode: 'nearest',
-            intersect: false,
-            },
+const Chart: NextPage<Props> = ({ data, usd }) => {
+
+function cssvar(name) {
+    return getComputedStyle(document.body).getPropertyValue(name);
+  }
+
+const options = {
+    responsive: true,
+    lineTension: 0.4,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+        display: false,
         },
-        scales: {
-            x: {
+        tooltip: {
+        displayColors: false,
+        // callbacks: {
+        //     title: function () {
+        //       return "my tittle";
+        //     }
+        //   }
+        mode: 'nearest',
+        intersect: false,
+        },
+    },
+    scales: {
+        x: {
+        grid: {
+            display: false,
+            drawBorder: true,
+            drawOnChartArea: true,
+            drawTicks: true,
+        },
+        ticks: {
+            color: cssvar('--primary-text-color'),
+        },
+        },
+        y: {
             grid: {
-                display: false,
-                drawBorder: true,
-                drawOnChartArea: true,
-                drawTicks: true,
+                drawBorder: false,
+                color: cssvar('--chart-line-color'),
             },
+            grace: '10%',
             ticks: {
                 color: cssvar('--primary-text-color'),
+                callback: function(value) {
+                return usd ? '$'+ value: value;
+            }
             },
-            },
-            y: {
-                grid: {
-                    drawBorder: false,
-                    color: cssvar('--chart-line-color'),
-                },
-                grace: '10%',
-                ticks: {
-                    color: cssvar('--primary-text-color'),
-                    callback: function(value) {
-                    return usd ? '$'+ value: value;
-                }
-                },
-                position: 'right',
-                // beginAtZero: true,
-            },
+            position: 'right',
+            // beginAtZero: true,
         },
-        };
+    },
+    };
 
   return <Line options={options} data={data} />
 }
+
+export default Chart
