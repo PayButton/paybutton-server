753dedc9f84983b194735e87ca5bd859ee35e9f9
chedieck <estevao@lobo.dev.br>
2022-07-29 10:35:41 -0300

    feat: seed mock paybuttons

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { chains } from './seeds/chains'
+import { paybuttons, addresses, connectors } from './seeds/paybuttons'
 import { createDevUserRawQueryList } from './seeds/devUser'
 const prisma = new PrismaClient()
 
@@ -7,6 +8,12 @@ async function main (): Promise<void> {
   // create chains
   if (await prisma.chain.count() === 0) {
     await prisma.chain.createMany({ data: chains })
+  }
+  // create paybuttons
+  if (await prisma.paybutton.count() === 0) {
+    await prisma.paybutton.createMany({ data: paybuttons })
+    await prisma.address.createMany({ data: addresses })
+    await prisma.addressesOnButtons.createMany({ data: connectors })
   }
   // create default dev user
   for (const q of createDevUserRawQueryList) {
