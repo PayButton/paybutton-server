e45ebbebc3444a7c8f1b4748ad0f3df756819d73
chedieck <estevao@lobo.dev.br>
2022-08-01 14:59:04 -0300

    refactor: `mockedAddress` -> `mockedBCHAddress` & create `mockedXECAddress`

--- tests/unittests/transactionsService.test.ts
+++ tests/unittests/transactionsService.test.ts
@@ -1,18 +1,18 @@
 import prisma from 'prisma/clientInstance'
 import * as transactionsService from 'services/transactionsService'
 import { prismaMock } from 'prisma/mockedClient'
-import { mockedTransaction, mockedGrpc, mockedAddress } from '../mockedObjects'
+import { mockedTransaction, mockedGrpc, mockedBCHAddress } from '../mockedObjects'
 
 describe('Create services', () => {
   it('Return created transaction', async () => {
     prismaMock.transaction.upsert.mockResolvedValue(mockedTransaction)
     prisma.transaction.upsert = prismaMock.transaction.upsert
-    prismaMock.address.findMany.mockResolvedValue([mockedAddress])
+    prismaMock.address.findMany.mockResolvedValue([mockedBCHAddress])
     prisma.address.findMany = prismaMock.address.findMany
 
     const result = await transactionsService.upsertTransaction(
       mockedGrpc.transaction1.toObject(),
-      mockedAddress.address
+      mockedBCHAddress.address
     )
     expect(result).toEqual(mockedTransaction)
   })
@@ -29,14 +29,14 @@ describe('Amount transactioned', () => {
   it('Negative transaction', async () => {
     const amount = await transactionsService.getTransactionAmount(
       mockedGrpc.transaction2.toObject(),
-      mockedAddress.address
+      mockedBCHAddress.address
     )
     expect(amount.toString()).toBe('-0.00000546')
   })
   it('Positive transaction', async () => {
     const amount = await transactionsService.getTransactionAmount(
       mockedGrpc.transaction1.toObject(),
-      mockedAddress.address
+      mockedBCHAddress.address
     )
     expect(amount.toString()).toBe('4.31247724')
   })
