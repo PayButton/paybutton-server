5e62083571ffd69b180a5c6b6d8825d9b3afb57a
chedieck <estevao@lobo.dev.br>
2022-08-01 15:03:13 -0300

    test: testing real & fake address on grpcService

--- tests/unittests/grpcService.test.ts
+++ tests/unittests/grpcService.test.ts
@@ -1,13 +1,13 @@
 import rewire from 'rewire'
-import { mockedGrpc } from '../mockedObjects'
+import { mockedGrpc, mockedBCHAddress, mockedXECAddress } from '../mockedObjects'
 const grpcService = rewire('../../services/grpcService')
 
 grpcService.__set__('grpcBCH', mockedGrpc)
 grpcService.__set__('grpcXEC', mockedGrpc)
 
 describe('Test service returned objects consistency', () => {
-  it('test getAddress', async () => {
-    const res = await grpcService.getAddress('mockaddress')
+  it('test getAddress for real address', async () => {
+    const res = await grpcService.getAddress(mockedBCHAddress.address)
     expect(res).toEqual(expect.objectContaining({
       confirmedTransactionsList: [
         mockedGrpc.transaction1.toObject(),
@@ -16,7 +16,7 @@ describe('Test service returned objects consistency', () => {
     }))
   })
   it('test getUtxos', async () => {
-    const res = await grpcService.getUtxos('mockaddress')
+    const res = await grpcService.getUtxos(mockedXECAddress.address)
     expect(res).toEqual(expect.objectContaining({
       outputsList: expect.arrayContaining([
         {
@@ -42,11 +42,11 @@ describe('Test service returned objects consistency', () => {
     }))
   })
   it('test getBCHBalance', async () => {
-    const res = await grpcService.getBCHBalance('mockaddress')
+    const res = await grpcService.getBCHBalance(mockedBCHAddress.address)
     expect(res).toBe(1780)
   })
   it('test getTransactionDetails', async () => {
-    const res = await grpcService.getTransactionDetails('mockaddress', 'bitcoincash')
+    const res = await grpcService.getTransactionDetails(mockedBCHAddress.address, 'bitcoincash')
     expect(res).toEqual(expect.objectContaining({
       transaction: {
         hash: 'hu9m3BZg/zlxis7ehc0x/+9qELXC8dkbimOtc5v598s=',
