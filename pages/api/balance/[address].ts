0b682975facb32f15e571bbd1951fd3390ff6ced
Andre Cabrera <git@andrecabrera.com>
2022-08-05 20:55:48 -0300

    fix: use parseAddress instead of default

--- pages/api/balance/[address].ts
+++ pages/api/balance/[address].ts
@@ -1,19 +1,20 @@
 import { NextApiResponse, NextApiRequest } from 'next'
 import { getBCHBalance } from 'services/bchdService'
 import { RESPONSE_MESSAGES } from 'constants/index'
+import { parseAddress } from 'utils/validators'
 import Cors from 'cors'
 
 const { ADDRESS_NOT_PROVIDED_400 } = RESPONSE_MESSAGES
 const cors = Cors({
-  methods: ['GET', 'HEAD'],
+  methods: ['GET', 'HEAD']
 })
 
-function runMiddleware(
+async function runMiddleware (
   req: NextApiRequest,
   res: NextApiResponse,
   fn: Function
-) {
-  return new Promise((resolve, reject) => {
+): Promise<void> {
+  return await new Promise((resolve, reject) => {
     fn(req, res, (result: any) => {
       if (result instanceof Error) {
         return reject(result)
@@ -24,14 +25,11 @@ function runMiddleware(
   })
 }
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void>  => {
-    await runMiddleware(req, res, cors)
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  await runMiddleware(req, res, cors)
   if (req.method === 'GET') {
-    const address = req.query.address as string
     try {
-      if (address === '' || address === undefined) {
-        throw new Error(ADDRESS_NOT_PROVIDED_400.message)
-      }
+      const address = parseAddress(req.query.address as string)
       const response = await getBCHBalance(address)
       res.status(200).send(response)
     } catch (err: any) {
