0ea1755cccaeae20a9fea72879e5c9ae16e2b9ee
chedieck <estevao@lobo.dev.br>
2022-08-26 10:28:09 -0300

    feat: using real data in dashboard, instead of dummy data

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FunctionComponent } from 'react'
+import React, { useState, useEffect, FunctionComponent } from 'react'
 import ThirdPartyEmailPassword from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
 import dynamic from 'next/dynamic'
 import supertokensNode from 'supertokens-node'
@@ -6,8 +6,6 @@ import * as SuperTokensConfig from '../../config/backendConfig'
 import Session from 'supertokens-node/recipe/session'
 import { GetServerSideProps } from 'next'
 import style from './dashboard.module.css'
-import dashboardData from './dummy-data.json'
-import moment from 'moment'
 const Chart = dynamic(async () => await import('./Chart'), {
   ssr: false
 })
@@ -24,70 +22,6 @@ const NumberBlock = ({ value, text }: NumberBlockProps): FunctionComponent<Numbe
       <h5>{text}</h5>
     </div>
   )
-}
-
-const initChartData = function (): any {
-  const yearLabels = [...new Array(12)].map((i, idx) => moment().startOf('day').subtract(idx, 'months').format('MMM'))
-  const thirtyDayLabels = [...new Array(30)].map((i, idx) => moment().startOf('day').subtract(idx, 'days').format('M/D'))
-  const sevenDayLabels = [...new Array(7)].map((i, idx) => moment().startOf('day').subtract(idx, 'days').format('M/D'))
-
-  return {
-    thirtyDayRevenue: {
-      labels: thirtyDayLabels.reverse(),
-      datasets: [
-        {
-          data: dashboardData.usdRevenueLast30days[1].data,
-          borderColor: '#669cfe'
-        }
-      ]
-    },
-
-    sevenDayRevenue: {
-      labels: sevenDayLabels.reverse(),
-      datasets: [
-        {
-          data: dashboardData.usdRevenueLast7days[1].data,
-          borderColor: '#669cfe'
-        }
-      ]
-    },
-    yearRevenue: {
-      labels: yearLabels,
-      datasets: [
-        {
-          data: dashboardData.usdRevenueLastYear[1].data,
-          borderColor: '#669cfe'
-        }
-      ]
-    },
-    yearPayments: {
-      labels: yearLabels.reverse(),
-      datasets: [
-        {
-          data: dashboardData.paymentsLastYear[1].data,
-          borderColor: '#66fe91'
-        }
-      ]
-    },
-    thirtyDayPayments: {
-      labels: thirtyDayLabels,
-      datasets: [
-        {
-          data: dashboardData.paymentsLast30days[1].data,
-          borderColor: '#66fe91'
-        }
-      ]
-    },
-    sevenDayPayments: {
-      labels: sevenDayLabels,
-      datasets: [
-        {
-          data: dashboardData.paymentsLast7days[1].data,
-          borderColor: '#66fe91'
-        }
-      ]
-    }
-  }
 }
 
 const ThirdPartyEmailPasswordAuthNoSSR = dynamic(
@@ -123,39 +57,50 @@ interface PaybuttonsProps {
 }
 
 export default function Dashboard ({ userId }: PaybuttonsProps): React.ReactElement {
-  const chartData = initChartData()
-  const [revenue, setRevenue] = useState(chartData.thirtyDayRevenue)
-  const [payments, setPayments] = useState(chartData.thirtyDayPayments)
+  const [dashboardData, setDashboardData] = useState()
+  const [period, setPeriod] = useState()
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const res = await fetch('api/dashboard')
+      const json = await res.json()
+      setDashboardData(json)
+      setPeriod(json.thirtyDays)
+    }
+    fetchData().catch(console.error)
+  }, [])
+
+  if (dashboardData === undefined || period === undefined) return <></>
+
   return (
     <ThirdPartyEmailPasswordAuthNoSSR>
       <h2>Dashboard</h2>
       <div className={style.number_ctn}>
-        <NumberBlock value={'$' + dashboardData.lifetimeRevenueUsd.toLocaleString()} text='Revenue (lifetime)' />
-        <NumberBlock value={dashboardData.lifetimePayments} text='Payments (lifetime)' />
-        <NumberBlock value={dashboardData.buttonsCount} text='Buttons' />
+        <NumberBlock value={'$'.concat(dashboardData.total.revenue) } text='Revenue (lifetime)' />
+        <NumberBlock value={dashboardData.total.payments} text='Payments (lifetime)' />
+        <NumberBlock value={dashboardData.total.buttons} text='Buttons' />
       </div>
       <div className={style.btn_ctn}>
-        <button className={revenue === chartData.sevenDayRevenue ? `${style.active_btn} ${style.toggle_btn}` : style.toggle_btn} onClick={() => { setRevenue(chartData.sevenDayRevenue); setPayments(chartData.sevenDayPayments) }}>1W</button>
-        <button className={revenue === chartData.thirtyDayRevenue ? `${style.active_btn} ${style.toggle_btn}` : style.toggle_btn} onClick={() => { setRevenue(chartData.thirtyDayRevenue); setPayments(chartData.thirtyDayPayments) }}>1M</button>
-        <button className={revenue === chartData.yearRevenue ? `${style.active_btn} ${style.toggle_btn}` : style.toggle_btn} onClick={() => { setRevenue(chartData.yearRevenue); setPayments(chartData.yearPayments) }}>1Y</button>
+        <button className={period === dashboardData.sevenDays ? `${style.active_btn} ${style.toggle_btn}` : style.toggle_btn} onClick={() => { setPeriod(dashboardData.sevenDays) }}>1W</button>
+        <button className={period === dashboardData.thirtyDays ? `${style.active_btn} ${style.toggle_btn}` : style.toggle_btn} onClick={() => { setPeriod(dashboardData.thirtyDays) }}>1M</button>
+        <button className={period === dashboardData.year ? `${style.active_btn} ${style.toggle_btn}` : style.toggle_btn} onClick={() => { setPeriod(dashboardData.year) }}>1Y</button>
       </div>
       <div className={style.chart_outer_ctn}>
         <div className={style.chart_inner_ctn}>
           <div className={style.chart_title_ctn}>
             <h4>Revenue</h4>
-            <h5>{revenue === chartData.yearRevenue ? 'Year' : revenue === chartData.thirtyDayRevenue ? '30 Day' : '7 Day'} Total: ${revenue === chartData.yearRevenue ? dashboardData.usdRevenueLastYear[0].total : revenue === chartData.thirtyDayRevenue ? dashboardData.usdRevenueLast30days[0].total : dashboardData.usdRevenueLast7days[0].total}</h5>
+            <h5>{period === dashboardData.year ? 'Year' : period === dashboardData.thirtyDays ? '30 Day' : '7 Day'} Total: ${period.totalRevenue}</h5>
           </div>
           <div className={style.chart_ctn}>
-            <Chart data={revenue} usd />
+            <Chart data={period.revenue} usd />
           </div>
         </div>
         <div className={style.chart_inner_ctn}>
           <div className={style.chart_title_ctn}>
             <h4>Payments</h4>
-            <h5>{payments === chartData.yearPayments ? 'Year' : payments === chartData.thirtyDayPayments ? '30 Day' : '7 Day'} Total: {payments === chartData.thirtyDayPayments ? dashboardData.paymentsLast30days[0].total : dashboardData.paymentsLast7days[0].total}</h5>
+            <h5>{period === dashboardData.year ? 'Year' : period === dashboardData.thirtyDays ? '30 Day' : '7 Day'} Total: {period.totalPayments}</h5>
           </div>
           <div className={style.chart_ctn}>
-            <Chart data={payments} />
+            <Chart data={period.payments} />
           </div>
         </div>
       </div>
