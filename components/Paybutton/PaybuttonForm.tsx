8e5aa7eebf5759f7a26c2b51f0667a66563f4a8c
John <jkuney17@gmail.com>
2022-09-01 12:25:49 -0500

    form ui

--- components/Paybutton/PaybuttonForm.tsx
+++ components/Paybutton/PaybuttonForm.tsx
@@ -8,14 +8,17 @@ import Plus from 'assets/plus.png'
 interface IProps {
   onSubmit: Function
   paybuttons: []
+  error: String
 }
 
-export default function PaybuttonForm ({ onSubmit, paybuttons }: IProps): ReactElement {
-  const { register, handleSubmit } = useForm<POSTParameters>()
+export default function PaybuttonForm ({ onSubmit, paybuttons, error }: IProps): ReactElement {
+  const { register, handleSubmit, reset } = useForm<POSTParameters>()
   const [modal, setModal] = useState(false)
+  const [multipleAddresses, setMultipleAddresses] = useState(false)
 
   useEffect(() => {
     setModal(false)
+    reset()
   }, [paybuttons])
 
   return (
@@ -33,20 +36,30 @@ export default function PaybuttonForm ({ onSubmit, paybuttons }: IProps): ReactE
             <h4>Create Button</h4>
             <div className={style.form_ctn}>
               <form onSubmit={handleSubmit(onSubmit)} method='post'>
-                <label htmlFor='name'>Name</label> <br />
+                <label htmlFor='name'>Name</label>
                 <input {...register('name')} type='text' id='name' name='name' required />
-                <br />
 
                 {/* <label htmlFor='buttonData'>Button Data</label> <br />
                 <textarea {...register('buttonData')} id='buttonData' name='buttonData' />
                 <br /> */}
 
-                <label htmlFor='addresses'>Addresses</label> <br />
-                <textarea {...register('addresses')} id='addresses' name='addresses' required />
-                <br />
+                <label className={style.labelMargin} htmlFor='addresses'>
+                  Address{multipleAddresses && 'es'}
+                  <div className={style.multiple_address} onClick={() => setMultipleAddresses(!multipleAddresses)}>
+                    {multipleAddresses ? 'Single Address' : 'Multiple Addresses'}
+                  </div>
+                </label>
+                {multipleAddresses
+                  ? <textarea {...register('addresses')} id='addresses' name='addresses' required />
+                  : <input {...register('addresses')} id='addresses' name='addresses' required />
+                }
+                {multipleAddresses &&
+                <div className={style.tip}>Place each address on a seperate line. No commas or spaces needed</div>
+                }
                 <div className={style.btn_row}>
-                  <button onClick={() => setModal(false)} className={style.cancel_btn}>Cancel</button>
+                  {error !== '' && <div className={style.error_message}>{error}</div>}
                   <button type='submit'>Submit</button>
+                  <button onClick={() => { setModal(false); reset() }} className={style.cancel_btn}>Cancel</button>
                 </div>
               </form>
             </div>
