21e5ae4d45a849fade0339d95db9b340990970d7
chedieck <estevao@lobo.dev.br>
2022-10-10 15:45:57 -0300

    test: add unittests for new services

--- tests/unittests/addressService.test.ts
+++ tests/unittests/addressService.test.ts
@@ -1,7 +1,8 @@
 import prisma from 'prisma/clientInstance'
+import { Prisma } from '@prisma/client'
 import * as addressService from 'services/addressService'
 import { prismaMock } from 'prisma/mockedClient'
-import { mockedBCHAddress } from '../mockedObjects'
+import { mockedBCHAddress, mockedNetwork, mockedTransactionList } from '../mockedObjects'
 import { RESPONSE_MESSAGES } from 'constants/index'
 
 describe('Find by substring', () => {
@@ -19,5 +20,17 @@ describe('Find by substring', () => {
     await expect(addressService.fetchAddressBySubstring('mock')).rejects.toThrow(
       RESPONSE_MESSAGES.NO_ADDRESS_FOUND_404.message
     )
+  })
+  it('Get address payment info', async () => {
+    jest.spyOn(addressService, 'fetchAddressBySubstring').mockImplementation(async (_: string) => {
+      return {
+        network: mockedNetwork,
+        transactions: mockedTransactionList,
+        ...mockedBCHAddress
+      }
+    })
+    const result = await addressService.getAddressPaymentInfo('mock')
+    expect(result).toHaveProperty('balance', new Prisma.Decimal('6.01247724'))
+    expect(result).toHaveProperty('paymentCount', 3)
   })
 })
