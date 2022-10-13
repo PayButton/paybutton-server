b1cd775e6edd0354e605d395b77b798876aa7503
chedieck <estevao@lobo.dev.br>
2022-10-13 15:47:07 -0300

    chore: add comment for wallet list sorting

--- pages/wallets/index.tsx
+++ pages/wallets/index.tsx
@@ -82,6 +82,9 @@ class ProtectedPage extends React.Component<WalletsProps, WalletsState> {
         <h2>Wallets</h2>
         <div>
         {this.state.walletsWithPaymentInfo.sort((a, b) => {
+          /* Sorts in the following order, from first to last, if they exist:
+           * Default XEC Wallet, Default BCH Wallet, other wallets.
+           */
           const aNetworkId = Number(a.wallet.userProfile?.isDefaultForNetworkId)
           const bNetworkId = Number(b.wallet.userProfile?.isDefaultForNetworkId)
           switch (aNetworkId | bNetworkId) {
