0cf91a7ba1a16d61b574236950f2b9224a184f56
chedieck <estevao@lobo.dev.br>
2022-10-24 15:27:47 -0300

    fix: input field className

--- components/Wallet/EditWalletForm.tsx
+++ components/Wallet/EditWalletForm.tsx
@@ -80,7 +80,7 @@ export default function EditWalletForm ({ wallet, userPaybuttons }: IProps): Rea
       <h4>Paybuttons</h4>
       <div className={s.buttonlist_ctn}>
       {userPaybuttons.map((pb, index) => (
-        <div key={pb.id} className={s.makedefault_ctn}>
+        <div key={pb.id} className={s.input_field}>
           <input {...register(`paybuttons.${index}`)}
           name={`paybuttons.${index}`}
           type='checkbox'
