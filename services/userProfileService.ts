102ba8e25aa9c2a82bbf0d769a9ab54d92056929
chedieck <estevao@lobo.dev.br>
2022-10-25 05:01:13 -0300

    fixup! Merge branch 'master' into feat/wallet-edit-back-end-defaults

--- services/userProfileService.ts
+++
@@ -1,8 +0,0 @@
-import { UserProfile } from '@prisma/client'
-import prisma from 'prisma/clientInstance'
-
-export async function fetchUserProfileByUserId (userId: string): Promise<UserProfile | null> {
-  return await prisma.userProfile.findUnique({
-    where: { userId }
-  })
-}
