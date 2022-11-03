1ccc124a9c5c6c8e0bf94bc13da9940bb0fc934b
chedieck <estevao@lobo.dev.br>
2022-11-03 11:17:01 -0300

    fix: app info format and price api url variable for tests

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,5 +1,5 @@
-const websiteBasePath: string = (process.env.WEBSITE_BASE_PATH as boolean) && (process.env.WEBSITE_BASE_PATH as string)
-const apiBasePath: string = (process.env.API_BASE_PATH as boolean) && (process.env.API_BASE_PATH as string)
+const websiteBasePath: string = process.env.WEBSITE_BASE_PATH as string
+const apiBasePath: string = process.env.API_BASE_PATH as string
 export const appInfo = {
   appName: process.env.APP_NAME,
   apiBasePath: `${apiBasePath}/auth`,
