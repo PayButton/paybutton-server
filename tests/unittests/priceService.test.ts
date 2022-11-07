e2a4c809ad26c72353460f0f9a2c07b45c7d7665
chedieck <estevao@lobo.dev.br>
2022-11-07 09:53:04 -0300

    tests: fix price tests

--- tests/unittests/priceService.test.ts
+++ tests/unittests/priceService.test.ts
@@ -1,19 +1,27 @@
 import * as priceService from 'services/priceService'
+import prisma from 'prisma/clientInstance'
 import { RESPONSE_MESSAGES } from 'constants/index'
 import { appInfo } from 'config/appInfo'
+import { prismaMock } from 'prisma/mockedClient'
+import { mockedUSDPrice, mockedCADPrice } from 'tests/mockedObjects'
 
 import axios from 'axios'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
-describe('', () => {
+describe('Sync price from transaction', () => {
+  beforeEach(async () => {
+    prismaMock.price.findMany.mockResolvedValue([])
+    prisma.price.findMany = prismaMock.price.findMany
+  })
   it('Ignore price API fail response', async () => {
     mockedAxios.get.mockResolvedValue({ data: { success: false } })
-    await priceService.syncTransactionPriceValues({
+    const res = await priceService.syncTransactionPriceValues({
       networkId: 1,
       transactionId: 1,
       timestamp: 1
     })
+    expect(res).toBeUndefined()
   })
   it('Fail if no PRICE_API_URL', async () => {
     appInfo.priceAPIURL = ''
@@ -27,5 +35,17 @@ describe('', () => {
     } catch (e: any) {
       expect(e.message).toMatch(RESPONSE_MESSAGES.MISSING_PRICE_API_URL_400.message)
     }
+    appInfo.priceAPIURL = 'foo'
+  })
+  it('Undefined if price already exists', async () => {
+    mockedAxios.get.mockResolvedValue({ data: { success: false } })
+    prismaMock.price.findMany.mockResolvedValue([mockedUSDPrice, mockedCADPrice])
+    prisma.price.findMany = prismaMock.price.findMany
+    const res = await priceService.syncTransactionPriceValues({
+      networkId: 1,
+      transactionId: 1,
+      timestamp: 1
+    })
+    expect(res).toBeUndefined()
   })
 })
