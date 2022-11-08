dd3f8c0f7a0f5351b173a4b06b91eec9f7dcbbef
John <jkuney17@gmail.com>
2022-11-08 16:18:03 -0600

    use new table on button detail page

--- components/Transaction/index.tsx
+++ components/Transaction/index.tsx
@@ -1,9 +1,5 @@
-import TransactionDetail from './TransactionDetail'
-import TransactionList from './TransactionList'
 import AddressTransactions from './AddressTransactions'
 
 export {
-  TransactionDetail,
-  TransactionList,
   AddressTransactions
 }
