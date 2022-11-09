3b832511d099583313459c26318ef1c69c7a9d6d
chedieck <estevao@lobo.dev.br>
2022-11-09 16:54:11 -0300

    feat: actually delete paybutton on detail view delete button

--- components/Paybutton/EditButtonForm.tsx
+++ components/Paybutton/EditButtonForm.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useState, useEffect } from 'react'
-import  { PaybuttonWithAddresses } from 'services/paybuttonService'
+import { PaybuttonWithAddresses } from 'services/paybuttonService'
 import { useForm } from 'react-hook-form'
 import { paybuttonPOSTParameters } from 'utils/validators'
 import Image from 'next/image'
@@ -19,8 +19,8 @@ interface IProps {
 export default function EditButtonForm ({ onSubmit, paybutton, error, onDelete }: IProps): ReactElement {
   const { register, handleSubmit, reset } = useForm<paybuttonPOSTParameters>()
   const [modal, setModal] = useState(false)
-  const [buttonname, setButtonName] = useState(paybutton.name)
-  const [deletemodal, setDeleteModal] = useState(false)
+  const [buttonName, setButtonName] = useState(paybutton.name)
+  const [deleteModal, setDeleteModal] = useState(false)
 
   useEffect(() => {
     setModal(false)
@@ -39,9 +39,9 @@ export default function EditButtonForm ({ onSubmit, paybutton, error, onDelete }
           <div className={style.form_ctn_inner}>
             <h4>Edit {paybutton.name}</h4>
             <div className={style.form_ctn}>
-              <form onSubmit={handleSubmit(onSubmit)} method='post'>
+              <form onSubmit={(e) => { void handleSubmit(onSubmit)(e) }} method='post'>
                 <label htmlFor='name'>Button Name</label>
-                <input {...register('name')} type='text' id='name' name='name' required placeholder={paybutton.name} value={buttonname} onChange={(e) => setButtonName(e.target.value)} />
+                <input {...register('name')} type='text' id='name' name='name' required placeholder={paybutton.name} value={buttonName} onChange={(e) => setButtonName(e.target.value)} />
                 <div className={style.btn_row2}>
                   {(error === undefined) ? null : <div className={style.error_message}>{error}</div>}
                   <button onClick={() => { setModal(false); reset(); setButtonName(paybutton.name); setDeleteModal(true) }} className={style.delete_btn}>Delete Button<div> <Image src={TrashIcon} alt='delete' /></div></button>
@@ -56,21 +56,20 @@ export default function EditButtonForm ({ onSubmit, paybutton, error, onDelete }
         </div>)
     : null}
 
-{deletemodal
+{deleteModal
   ? (
         <div className={style.form_ctn_outer}>
           <div className={style.form_ctn_inner}>
             <h4>Delete {paybutton.name}?</h4>
             <div className={`${style.form_ctn} ${style.delete_button_form_ctn}`}>
-              <form onSubmit={handleSubmit(onSubmit)} method='post'>
                 <label htmlFor='name'>Are you sure you want to delete {paybutton.name}?<br />This action cannot be undone.</label>
                 <div className={style.btn_row}>
                   <div>
-                    <button type='submit' className={style.delete_confirm_btn}>Yes, Delete This Button</button>
+
+                    <button onClick={() => { onDelete(paybutton.id) }} className={style.delete_confirm_btn}>Yes, Delete This Button</button>
                     <button onClick={() => { setDeleteModal(false); reset(); setModal(true) }} className={style.cancel_btn}>Cancel</button>
                   </div>
                 </div>
-              </form>
             </div>
           </div>
         </div>)
