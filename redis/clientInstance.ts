d6155e8ad8e8a651ed7a80a1b3961de339f8c492
chedieck <estevao@lobo.dev.br>
2022-11-29 19:28:32 -0300

    refactor: redis URL in .env.development

--- redis/clientInstance.ts
+++ redis/clientInstance.ts
@@ -1,6 +1,7 @@
 import { createClient } from 'redis'
+import { appInfo } from 'config/appInfo'
 
-export const client = createClient({ url: 'redis://paybutton-cache:6379' })
+export const client = createClient({ url: appInfo.redisURL })
 client.on('error', (err) => console.log('Redis Client Error', err))
 
 void client.connect()
