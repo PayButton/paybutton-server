71ab69bbcc81eec8a397006e2a5dd6e1919c3ef9
chedieck <estevao@lobo.dev.br>
2023-01-03 15:58:30 -0300

    refactor: create dedicated script for initializing jobs

--- scripts/paybutton-server-start.sh
+++ scripts/paybutton-server-start.sh
@@ -3,6 +3,5 @@
 yarn || exit
 yarn prisma migrate dev || exit
 yarn prisma db seed || exit
-date "+start date: %Y-%m-%d %H:%M:%S" > jobs/out.log || exit
-tmux new-session -d -s "initJobs" 'yarn initJobs 2>&1 | tee -a jobs/out.log' || exit
+./init-jobs.sh || exit
 yarn dev || exit
