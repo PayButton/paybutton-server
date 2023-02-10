0fca2e89375fac26f85ce2d483898787edd8b09a
chedieck <estevao@lobo.dev.br>
2023-02-10 16:37:36 +0100

    feat: endpoint to fetch user addresses

---
+++ pages/api/addresses/index.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import * as addressService from 'services/addressService'
+import { RESPONSE_MESSAGES } from 'constants/index'
+
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  if (req.method === 'GET') {
+    const query = req.query
+    const userId: string | string[] | undefined = query.userId
+    try {
+      if (userId === '' || userId === undefined) throw new Error(RESPONSE_MESSAGES.USER_ID_NOT_PROVIDED_400.message)
+      if (Array.isArray(userId)) throw new Error(RESPONSE_MESSAGES.MULTIPLE_USER_IDS_PROVIDED_400.message)
+      const addressList = await addressService.fetchAllUserAddresses(userId)
+      res.status(200).json(addressList)
+    } catch (err: any) {
+      switch (err.message) {
+        case RESPONSE_MESSAGES.USER_ID_NOT_PROVIDED_400.message:
+          res.status(400).json(RESPONSE_MESSAGES.USER_ID_NOT_PROVIDED_400)
+          break
+        case RESPONSE_MESSAGES.MULTIPLE_USER_IDS_PROVIDED_400.message:
+          res.status(400).json(RESPONSE_MESSAGES.MULTIPLE_USER_IDS_PROVIDED_400)
+          break
+        default:
+          res.status(500).json({ statusCode: 500, message: err.message })
+      }
+    }
+  }
+}
