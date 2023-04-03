1f71118074baaaea24c6d1ad4f970f6ff58044b8
chedieck <estevao@lobo.dev.br>
2023-04-03 14:48:54 +0300

    feat: address uuid on seeds

--- prisma/seeds/addresses.ts
+++ prisma/seeds/addresses.ts
@@ -1,21 +1,21 @@
 export const addresses = [
   {
-    id: 1,
+    id: '0a03a880-86fe-4d82-9aa2-8df270cf032d',
     address: 'ecash:qrmm7edwuj4jf7tnvygjyztyy0a0qxvl7quss2vxek',
     networkId: 1
   },
   {
-    id: 2,
+    id: 'a37b9a8c-d262-468b-b1dd-571434a16308',
     address: 'bitcoincash:qzqh7rwaq9zm4zcv40lh9c9u50gy07gcesdmja8426',
     networkId: 2
   },
   {
-    id: 3,
+    id: '1ca6b7f5-6930-42a7-8ea4-8de57de03251',
     address: 'ecash:qp96jaej87uha99c2hyxh4tqc0wwde5g2v3mm9yd9x',
     networkId: 1
   },
   {
-    id: 4,
+    id: '4f68e74f-de19-467a-b195-139d98217ada',
     address: 'ecash:qzep8jedy9mgx2h6tspte80lqssaudu0pve4fr9dk5',
     networkId: 1
   }
