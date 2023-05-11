084e7b789833e33f5c686e2377d177ce2518d391
chedieck <estevao@lobo.dev.br>
2023-05-11 16:26:48 +0300

    temp: remove BCH address from seeds

--- prisma/seeds/addressUserConnectors.ts
+++ prisma/seeds/addressUserConnectors.ts
@@ -4,11 +4,11 @@ export const addressUserConnectors = [
     addressId: '0a03a880-86fe-4d82-9aa2-8df270cf032d',
     userId: 'dev-uid'
   },
-  {
-    walletId: '80fcbeb5-f6bd-4b3b-aca8-d604a670e978',
-    addressId: 'a37b9a8c-d262-468b-b1dd-571434a16308',
-    userId: 'dev-uid'
-  },
+  // {
+  // walletId: '80fcbeb5-f6bd-4b3b-aca8-d604a670e978',
+  // addressId: 'a37b9a8c-d262-468b-b1dd-571434a16308',
+  // userId: 'dev-uid'
+  // },
   {
     walletId: '408093c4-2eff-4b35-a0c5-c369220ca236',
     addressId: '1ca6b7f5-6930-42a7-8ea4-8de57de03251',
