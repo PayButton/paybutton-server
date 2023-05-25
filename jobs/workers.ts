83b8bc482047c681c27dcca609bc0d08cf3f8983
chedieck <estevao@lobo.dev.br>
2023-05-25 12:12:12 +0300

    feat: better error logs for failed job sync

--- jobs/workers.ts
+++ jobs/workers.ts
@@ -1,7 +1,7 @@
 import { Worker, Job, Queue } from 'bullmq'
 import { Address } from '@prisma/client'
 import { redisBullMQ } from 'redis/clientInstance'
-import { SYNC_NEW_ADDRESSES_DELAY, DEFAULT_WORKER_LOCK_DURATION, RESPONSE_MESSAGES } from 'constants/index'
+import { SYNC_NEW_ADDRESSES_DELAY, DEFAULT_WORKER_LOCK_DURATION, RESPONSE_MESSAGES, KeyValueT } from 'constants/index'
 
 import * as transactionService from 'services/transactionService'
 import * as priceService from 'services/priceService'
@@ -9,34 +9,34 @@ import * as addressService from 'services/addressService'
 import { subscribeAddressesAddTransactions } from 'services/blockchainService'
 import { parseError } from 'utils/validators'
 
-const syncAndSubscribeAddresses = async (addresses: Address[]): Promise<string[]> => {
-  const failedAddresses: string[] = []
+const syncAndSubscribeAddresses = async (addresses: Address[]): Promise<KeyValueT<string>> => {
+  const failedAddressesWithError: KeyValueT<string> = {}
   await Promise.all(
     addresses.map(async (addr) => {
       try {
         await subscribeAddressesAddTransactions([addr])
         await transactionService.syncAllTransactionsForAddress(addr.address, Infinity)
       } catch (err: any) {
-        failedAddresses.push(addr.address)
+        failedAddressesWithError[addr.address] = err.toString()
       }
     })
   )
-  return failedAddresses
+  return failedAddressesWithError
 }
 
 const syncAndSubscribeAllAddressTransactionsForNetworkJob = async (job: Job): Promise<void> => {
   console.log(`job ${job.id as string}: syncing and subscribing all addresses for network ${job.data.networkId as string}...`)
-  let failedAddresses: string [] = []
+  let failedAddressesWithError: KeyValueT<string> = {}
   try {
     const addresses = await addressService.fetchAllAddressesForNetworkId(job.data.networkId)
-    failedAddresses = await syncAndSubscribeAddresses(addresses)
+    failedAddressesWithError = await syncAndSubscribeAddresses(addresses)
   } catch (err: any) {
     const parsedError = parseError(err)
     if (parsedError.message === RESPONSE_MESSAGES.TRANSACTION_ALREADY_EXISTS_FOR_ADDRESS_400.message) {
       console.log(`initial syncing of network ${job.data.networkId as string} encountered known transaction, skipping...`)
     } else {
-      if (failedAddresses.length > 0) {
-        console.error(`ERROR: (skipping anyway) initial syncing of network ${job.data.networkId as string} FAILED for addresses ${JSON.stringify(failedAddresses)}: ${err.message as string}`)
+      if (Object.keys(failedAddressesWithError).length > 0) {
+        console.error(`ERROR: (skipping anyway) initial syncing of network ${job.data.networkId as string} FAILED for addresses ${JSON.stringify(failedAddressesWithError)}: ${err.message as string}`)
       } else {
         console.error(`ERROR: (skipping anyway) initial syncing of network ${job.data.networkId as string} FAILED: ${err.message as string}`)
       }
@@ -104,10 +104,10 @@ export const syncAndSubscribeUnsyncedAddressesWorker = async (queue: Queue): Pro
       const newAddresses = await addressService.fetchUnsyncedAddresses()
       if (newAddresses.length !== 0) {
         const failedAddresses = await syncAndSubscribeAddresses(newAddresses)
-        if (failedAddresses.length > 0) {
+        if (Object.keys(failedAddresses).length > 0) {
           console.error(`automatic syncing of addresses failed for addresses: ${JSON.stringify(failedAddresses)}`)
         }
-        job.data.syncedAddresses = newAddresses.filter(addr => !failedAddresses.includes(addr.address))
+        job.data.syncedAddresses = newAddresses.filter(addr => addr.address in failedAddresses)
       }
 
       // add same job to the queue again, so it runs repeating
