c24a364221f703b6deb1cd8e8ea40465a78173da
chedieck <estevao@lobo.dev.br>
2023-05-30 12:16:14 +0300

    fix: interpret commands with ash and read .env.local

--- scripts/paybutton-server-start.sh
+++ scripts/paybutton-server-start.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 source .env
+source .env.local
+
 chmod 2755 /home/node/.pm2/logs
 chown node:node /home/node/.pm2/logs
 
 yarn || exit 1
 yarn prisma migrate dev || exit 1
 yarn prisma db seed || exit 1
-pm2 start yarn --name jobs -- initJobs || exit 1
-pm2 start yarn --name SSEServer -- initSSEServer || exit 1
+pm2 start yarn --interpreter ash --name jobs -- initJobs || exit 1
+pm2 start yarn --interpreter ash --name SSEServer -- initSSEServer || exit 1
 
 if [[ "$ENVIRONMENT" == "prod" ]]; then
-    pm2 start yarn --name next -- prod || exit 1
+    pm2 start yarn --interpreter ash --name next -- prod || exit 1
 else
-    pm2 start yarn --name next -- dev || exit 1
+    pm2 start yarn --interpreter ash --name next -- dev || exit 1
 fi
 
 tail -f /dev/null
