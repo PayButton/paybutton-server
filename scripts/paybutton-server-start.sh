9b7477835bee010040ba71ece44d5b440ee8b634
chedieck <estevao@lobo.dev.br>
2023-05-30 13:25:52 +0300

    fix: expect 'production' not 'prod'

--- scripts/paybutton-server-start.sh
+++ scripts/paybutton-server-start.sh
@@ -8,8 +8,8 @@ yarn prisma db seed || exit 1
 pm2 start yarn --time --interpreter ash --name jobs -- initJobs || exit 1
 pm2 start yarn --time --interpreter ash --name SSEServer -- initSSEServer || exit 1
 
-if [[ "$ENVIRONMENT" == "prod" ]]; then
-    pm2 start yarn --time --interpreter ash --name next -- prod || exit 1
+if [[ "$ENVIRONMENT" == "production" ]]; then
+    pm2 start yarn --time --interpreter ash --name next -- build || exit 1
 else
     pm2 start yarn --time --interpreter ash --name next -- dev || exit 1
 fi
