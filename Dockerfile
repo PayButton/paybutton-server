cdf60d88d6966e53adf42877da5fa2cfd5e9e137
chedieck <estevao@lobo.dev.br>
2023-05-30 11:45:23 +0300

    feat: using pm2 instead of tmux & allowing to read ENVIRONMENT=prod

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM  --platform=linux/amd64 node:lts-alpine
 RUN apk add --no-cache python3 make g++ tmux openssl1.1-compat
+RUN yarn global add pm2
 USER node
 WORKDIR /home/node/src/
-ENV PATH /home/node/src/node_modules/.bin:$PATH
+ENV PATH /home/node/src/node_modules/.bin:/home/node/.yarn/bin:$PATH
 EXPOSE 3000
 EXPOSE 5000
--- scripts/paybutton-server-start.sh
+++ scripts/paybutton-server-start.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+source .env
 
 yarn || exit 1
 yarn prisma migrate dev || exit 1
 yarn prisma db seed || exit 1
-sh scripts/init-jobs.sh || exit 1
-sh scripts/init-sse-server.sh || exit 1
-yarn dev || exit 1
+pm2 start yarn --name jobs -- initJobs || exit 1
+pm2 start yarn --name SSEServer -- initSSEServer || exit 1
+
+if [[ "$ENVIRONMENT" == "prod" ]]; then
+    pm2 start yarn --name next -- prod || exit 1
+else
+    pm2 start yarn --name next -- dev || exit 1
+fi
+
+tail -f /dev/null
