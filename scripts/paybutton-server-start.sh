c8f6e9cb45dace073d3ba019f11f63e064aea0d4
chedieck <estevao@lobo.dev.br>
2023-05-31 14:14:50 +0300

    fix: make init script posix compatible

--- scripts/paybutton-server-start.sh
+++ scripts/paybutton-server-start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-source .env
-source .env.local
+
+. .env
+. .env.local
 
 yarn || exit 1
 yarn prisma migrate dev || exit 1
@@ -8,8 +9,8 @@ yarn prisma db seed || exit 1
 pm2 start yarn --time --interpreter ash --name jobs -- initJobs || exit 1
 pm2 start yarn --time --interpreter ash --name SSEServer -- initSSEServer || exit 1
 
-if [[ "$ENVIRONMENT" == "production" ]]; then
-    pm2 start yarn --time --interpreter ash --name next -- build || exit 1
+if [ "$ENVIRONMENT" = "production" ]; then
+    pm2 start yarn --time --interpreter ash --name next -- prod || exit 1
 else
     pm2 start yarn --time --interpreter ash --name next -- dev || exit 1
 fi
