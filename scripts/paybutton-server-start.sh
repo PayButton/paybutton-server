2d1c185c52e2ad622f454e25f3bbd0a3afcd50c3
chedieck <estevao@lobo.dev.br>
2023-06-01 18:16:42 +0300

    fix: try removing empty lines

--- scripts/paybutton-server-start.sh
+++ scripts/paybutton-server-start.sh
@@ -3,20 +3,16 @@ echo pwd
 pwd
 echo ls
 ls
-
 . .env
 . .env.local
-
 yarn || exit 1
 yarn prisma migrate dev || exit 1
 yarn prisma db seed || exit 1
 pm2 start yarn --time --interpreter ash --name jobs -- initJobs || exit 1
 pm2 start yarn --time --interpreter ash --name SSEServer -- initSSEServer || exit 1
-
 if [ "$ENVIRONMENT" = "production" ]; then
     pm2 start yarn --time --interpreter ash --name next -- prod || exit 1
 else
     pm2 start yarn --time --interpreter ash --name next -- dev || exit 1
 fi
-
 tail -f /dev/null
