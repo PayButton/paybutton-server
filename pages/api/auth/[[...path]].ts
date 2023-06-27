588063872dfe7b30a7772080bd54e0504cbb7b64
chedieck <estevao@lobo.dev.br>
2023-06-27 13:52:03 +0300

    fix: infinite verification email loop

--- pages/api/auth/[[...path]].ts
+++ pages/api/auth/[[...path]].ts
@@ -9,6 +9,10 @@ supertokens.init(SuperTokensConfig.backendConfig())
 export default async function superTokens (req: Request, res: Response): Promise<void> {
   await superTokensNextWrapper(
     async (next) => {
+      res.setHeader(
+        'Cache-Control',
+        'no-cache, no-store, max-age=0, must-revalidate'
+      )
       await middleware()(req, res, next)
     },
     req,
