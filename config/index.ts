01021d8108891a502514f89711c6f1165461a258
chedieck <estevao@lobo.dev.br>
2023-06-29 16:22:14 +0300

    refactor: config from appInfo to `paybutton-config.json`

---
+++ config/index.ts
@@ -0,0 +1,38 @@
+import localConfig from '../paybutton-config.json'
+
+interface Config {
+  appName?: string
+  apiDomain: string
+  websiteDomain: string
+
+  sseBaseURL: string
+  sseAuthKey: string
+  showTestNetworks: false
+  grpcBCHNodeURL: string
+  grpcBCHNodeTestURL: string
+  grpcXECNodeURL: string
+  priceAPIURL: string
+  priceAPIToken: string
+  redisURL: string
+}
+
+const readConfig = (): Config => {
+  config = localConfig as Config
+  config.appName = 'PayButton'
+  return config
+}
+
+let config: Config
+
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  config: Config
+}
+declare const global: CustomNodeJsGlobal
+
+if (global.config === undefined) {
+  global.config = readConfig()
+}
+
+config = global.config
+
+export default config
