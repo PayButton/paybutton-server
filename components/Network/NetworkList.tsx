6c60285be0b2d280b27bf6e7088f2a1a1b5b5277
chedieck <estevao@lobo.dev.br>
2023-06-30 17:02:18 +0300

    refactor: remove duplicade code

--- components/Network/NetworkList.tsx
+++ components/Network/NetworkList.tsx
@@ -20,48 +20,24 @@ const NetworkComponent: FunctionComponent<IProps> = ({
   return (
     <div className={style.network_ctn}>
       {networks.map((network) => {
-        if (userNetworks.some((n) => n.ticker === network.ticker) || userNetworks.length === 0) {
-          return (
-            <div key={network.id} className={style.card_wrapper}>
-              <div className={style.network_card_text}>
-                <div className={style.cardTitle}>{network.title}</div>
-                {network.connected
-                  ? (
-                  <>
-                    <div
-                      className={style.cardStatus}
-                      style={{ color: '#04b504' }}
-                    >
-                      Connected
-                    </div>
-                    <div>
-                      Last block:{' '}
-                      {network.lastBlockTimestamp !== undefined
-                        ? moment.unix(network.lastBlockTimestamp).fromNow()
-                        : '-'}
-                    </div>
-                  </>
-                    )
+        return (
+          <div
+            key={network.id}
+            className={style.card_wrapper}
+
+            style={
+              (userNetworks.some((n) => n.ticker === network.ticker) || userNetworks.length === 0)
+                ? {}
+                : show ? { order: '2', display: 'inline-block' } : { order: '2', display: 'none' }}
+          >
+            <div className={style.network_card_text}>
+              <div className={style.cardTitle}>{network.title}</div>
+              {
+                network.maintenance
+                  ? <div className={style.cardStatus}>Under Maintenance</div>
                   : (
-                      network.maintenance
-                        ? <div className={style.cardStatus}>Under Maintenance</div>
-                        : <div className={style.cardStatus}>Disconnected</div>
-                    )}
-              </div>
-            </div>
-          )
-        } else {
-          return (
-              <div
-                key={network.id}
-                className={style.card_wrapper}
-                style={show ? { order: '2', display: 'inline-block' } : { order: '2', display: 'none' }}
-              >
-                <div className={style.network_card_text}>
-                  <div className={style.cardTitle}>{network.title}</div>
-                  {
-                    network.connected
-                      ? (
+                      network.connected
+                        ? (
                     <>
                       <div
                         className={style.cardStatus}
@@ -76,24 +52,21 @@ const NetworkComponent: FunctionComponent<IProps> = ({
                           : '-'}
                       </div>
                     </>
-                        )
-                      : (
-                          network.maintenance
-                            ? <div className={style.cardStatus}>Under Maintenance</div>
-                            : <div className={style.cardStatus}>Disconnected</div>
-                        )}
-                </div>
-              </div>
-          )
-        }
+                          )
+                        : <div className={style.cardStatus}>Disconnected</div>
+                    )
+              }
+            </div>
+          </div>
+        )
       })}
       {userNetworks.length < 2 && userNetworks.length !== 0 &&
-       <div
-        className={style.showNetworksCtn}
-        onClick={() => setShow(!show)}
-      >
-        {!show ? 'Show Other Networks' : 'Hide Other Networks'}
-      </div>
+        <div
+          className={style.showNetworksCtn}
+          onClick={() => setShow(!show)}
+        >
+          {!show ? 'Show Other Networks' : 'Hide Other Networks'}
+        </div>
       }
     </div>
   )
