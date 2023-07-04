49fb34f0ecebea7ad1551fa05f70d69164d3bce1
chedieck <estevao@lobo.dev.br>
2023-07-04 15:11:22 +0300

    feat: differentiate protocols in prod and dev

--- config/index.ts
+++ config/index.ts
@@ -29,6 +29,12 @@ const readConfig = (): Config => {
   if (config.networksUnderMaintenance === undefined) {
     config.networksUnderMaintenance = {}
   }
+  if (process.env.NODE_ENV === 'production') {
+    config.wsBaseURL = `wss://${config.wsBaseURL}`
+  } else {
+    config.wsBaseURL = `http://${config.wsBaseURL}`
+  }
+
   return config
 }
 
