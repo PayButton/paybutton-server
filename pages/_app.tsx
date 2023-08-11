f5787b90cd99f256e63f7504f39f5a878c6453aa
chedieck <estevao@lobo.dev.br>
2023-08-11 17:51:53 -0300

    fix: redirect when not logged in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,8 @@ import 'styles/variables.css'
 import 'styles/global.css'
 import React, { useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/navigation'
 import SuperTokensWebJs from 'supertokens-web-js'
 import * as SuperTokensConfig from '../config/frontendConfig'
-import Session from 'supertokens-auth-react/recipe/session'
 import ErrorBoundary from 'components/ErrorBoundary'
 import Head from 'next/head'
 import Page from 'components/Page'
@@ -17,32 +15,18 @@ if (typeof window !== 'undefined') {
 }
 
 function App ({ Component, pageProps }: AppProps): React.ReactElement | null {
-  const router = useRouter()
-  async function redirectIfNotSignedIn (): Promise<void> {
-    if (!(await SessionJS.doesSessionExist())) {
-      router.push('/signin')
-    }
-  }
-
   const [chart, setChart] = useState(true)
 
   useEffect(() => {
-    async function doRefresh (): Promise<void> {
-      await redirectIfNotSignedIn()
-      if (pageProps.fromSupertokens === 'needs-refresh') {
-        if (await Session.attemptRefreshingSession()) {
-          location.reload()
-        } else {
-          // user has been logged out
-          router.push('/signin')
-        }
+    void (async () => {
+      if (
+        !(await SessionJS.doesSessionExist()) &&
+        window.location.pathname !== '/signin') {
+        window.location.href = '/signin'
       }
-    }
-    void doRefresh()
-  }, [pageProps.fromSupertokens])
-  if (pageProps.fromSupertokens === 'needs-refresh') {
-    return null
-  }
+    })()
+  })
+
   return (
     <>
       <Head>
