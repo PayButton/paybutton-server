6ca4b61747ad6b105e972301b687367af99575a1
John <jkuney17@gmail.com>
2023-08-15 10:42:48 -0500

    add copy button, and better button image

--- components/LandingPage/GetStarted.tsx
+++ components/LandingPage/GetStarted.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import style from '/styles/landing.module.css';
+import CopyIcon from '/assets/copy.png';
 
 export default function GetStarted(): JSX.Element {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyClick = async (): Promise<void> => {
+    const codeToCopy = `<script src="https://unpkg.com/@paybutton/paybutton/dist/paybutton.js"></script>
+    <div class="paybutton" to="YOUR_ADDRESS_HERE"></div>`;
+
+    try {
+      await navigator.clipboard.writeText(codeToCopy);
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1000); // Reset the "Copied" state after 2 seconds
+    } catch (error) {
+      console.error('Copy failed:', error);
+    }
+  };
+
   return (
     <div className={style.get_started_ctn}>
       <div className={style.container}>
@@ -10,6 +30,12 @@ export default function GetStarted(): JSX.Element {
           Simply add the following to your website’s HTML, replacing{' '}
           <i>YOUR_ADDRESS_HERE</i> with your eCash address.
         </p>
+        <div className={style.codetopbar}>
+          <div className={style.copybutton} onClick={handleCopyClick}>
+            <Image src={CopyIcon} alt="copy" />
+            {isCopied ? 'Copied!' : 'Copy'}
+          </div>
+        </div>
         <pre>
           <code>
             &lt;script
