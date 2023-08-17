42e650ec83171fa60bb033ede0c91ecfe9d5047f
John <jkuney17@gmail.com>
2023-08-17 16:18:33 -0500

    account ui

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,52 +1,66 @@
-import React from 'react'
-import ThirdPartyEmailPasswordNode from 'supertokens-node/recipe/thirdpartyemailpassword'
-import supertokensNode from 'supertokens-node'
-import * as SuperTokensConfig from '../../config/backendConfig'
-import Session from 'supertokens-node/recipe/session'
-import { GetServerSideProps } from 'next'
-import Page from 'components/Page'
-import ChangePassword from 'components/Account/ChangePassword'
+import React, { useState } from 'react';
+import ThirdPartyEmailPasswordNode from 'supertokens-node/recipe/thirdpartyemailpassword';
+import supertokensNode from 'supertokens-node';
+import * as SuperTokensConfig from '../../config/backendConfig';
+import Session from 'supertokens-node/recipe/session';
+import { GetServerSideProps } from 'next';
+import Page from 'components/Page';
+import ChangePassword from 'components/Account/ChangePassword';
+import style from './account.module.css';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // this runs on the backend, so we must call init on supertokens-node SDK
-  supertokensNode.init(SuperTokensConfig.backendConfig())
-  let session
+  supertokensNode.init(SuperTokensConfig.backendConfig());
+  let session;
   try {
-    session = await Session.getSession(context.req, context.res)
+    session = await Session.getSession(context.req, context.res);
   } catch (err: any) {
     if (err.type === Session.Error.TRY_REFRESH_TOKEN) {
-      return { props: { fromSupertokens: 'needs-refresh' } }
+      return { props: { fromSupertokens: 'needs-refresh' } };
     } else if (err.type === Session.Error.UNAUTHORISED) {
-      return { props: {} }
+      return { props: {} };
     } else {
-      throw err
+      throw err;
     }
   }
-  if (session === undefined) return
-  const userId = session?.getUserId()
+  if (session === undefined) return;
+  const userId = session?.getUserId();
 
   return {
     props: {
       userId,
-      user: await ThirdPartyEmailPasswordNode.getUserById(userId)
-    }
-  }
-}
+      user: await ThirdPartyEmailPasswordNode.getUserById(userId),
+    },
+  };
+};
 
 interface IProps {
-  userId: string
-  user: ThirdPartyEmailPasswordNode.User | undefined
+  userId: string;
+  user: ThirdPartyEmailPasswordNode.User | undefined;
 }
 
-export default function Account ({ user, userId }: IProps): React.ReactElement {
+export default function Account({ user, userId }: IProps): React.ReactElement {
+  const [changePassword, setChangePassword] = useState(false);
   if (user !== null) {
     return (
-      <>
+      <div className={style.account_ctn}>
         <h2>Account</h2>
-        <p>Email: {user.email}</p>
-        <ChangePassword/>
-      </>
-    )
+        <div className={style.label}>Email</div>
+        <div className={style.account_card}>{user.email}</div>
+        <div
+          onClick={() => setChangePassword(!changePassword)}
+          className={style.updatebtn}
+        >
+          {!changePassword ? 'Update Password' : 'Cancel'}
+        </div>
+        {changePassword && (
+          <>
+            <div className={style.label}>Update Password</div>
+            <ChangePassword />
+          </>
+        )}
+      </div>
+    );
   }
-  return <Page/>
+  return <Page />;
 }
