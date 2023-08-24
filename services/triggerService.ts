2952619c3382d2a282870c26ec2cebecb08f90a3
chedieck <estevao@lobo.dev.br>
2023-08-24 15:41:34 -0300

    feat: block multiple triggers per addr

--- services/triggerService.ts
+++ services/triggerService.ts
@@ -1,7 +1,7 @@
 import { PaybuttonTrigger } from '@prisma/client'
 import { RESPONSE_MESSAGES } from 'constants/index'
 import prisma from 'prisma/clientInstance'
-import { fetchPaybuttonById } from './paybuttonService'
+import { fetchPaybuttonById, fetchPaybuttonWithTriggers } from './paybuttonService'
 
 export interface CreatePaybuttonTriggerInput {
   userId: string
@@ -10,10 +10,24 @@ export interface CreatePaybuttonTriggerInput {
   postData?: string
 }
 
+export async function fetchTriggersForPaybutton (paybuttonId: string, userId: string): Promise<PaybuttonTrigger[]> {
+  const paybutton = await fetchPaybuttonWithTriggers(paybuttonId)
+  if (paybutton.providerUserId !== userId) {
+    throw new Error(RESPONSE_MESSAGES.RESOURCE_DOES_NOT_BELONG_TO_USER_400.message)
+  }
+  return paybutton.triggers
+}
+
 export async function createTrigger (paybuttonId: string, values: CreatePaybuttonTriggerInput): Promise<PaybuttonTrigger> {
-  const paybutton = await fetchPaybuttonById(paybuttonId)
+  const paybutton = await fetchPaybuttonWithTriggers(paybuttonId)
   if (paybutton.providerUserId !== values.userId) {
     throw new Error(RESPONSE_MESSAGES.RESOURCE_DOES_NOT_BELONG_TO_USER_400.message)
+  }
+  if (paybutton.triggers.length !== 0) {
+    throw new Error(RESPONSE_MESSAGES.LIMIT_TRIGGERS_PER_BUTTON_400.message)
+  }
+  if ((await fetchTriggersForPaybuttonAddresses(paybutton.id)).length > 0) {
+    throw new Error(RESPONSE_MESSAGES.LIMIT_TRIGGERS_PER_BUTTON_ADDRESSES_400.message)
   }
   return await prisma.paybuttonTrigger.create({
     data: {
@@ -21,6 +35,28 @@ export async function createTrigger (paybuttonId: string, values: CreatePaybutto
       sendEmail: values.sendEmail,
       postURL: values.postURL ?? '',
       postData: values.postData ?? ''
+    }
+  })
+}
+
+export async function fetchTriggersForPaybuttonAddresses (paybuttonId: string): Promise<PaybuttonTrigger[]> {
+  const paybutton = await fetchPaybuttonById(paybuttonId)
+  const addressStringList = paybutton.addresses.map((conn) => conn.address.address)
+  const userId = paybutton.providerUserId
+  return await prisma.paybuttonTrigger.findMany({
+    where: {
+      paybutton: {
+        addresses: {
+          some: {
+            address: {
+              address: {
+                in: addressStringList
+              }
+            }
+          }
+        },
+        providerUserId: userId
+      }
     }
   })
 }
