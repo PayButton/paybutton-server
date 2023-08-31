5e77e41d2b46e039aa7a74d5b939e2e82d7e1dd9
chedieck <estevao@lobo.dev.br>
2023-08-31 17:24:33 -0300

    fix: set triggerId null on log if deleted

--- prisma/migrations/20230831194209_trigger_logs/migration.sql
+++ prisma/migrations/20230831194209_trigger_logs/migration.sql
@@ -12,4 +12,4 @@ CREATE TABLE `TriggerLog` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `TriggerLog` ADD CONSTRAINT `TriggerLog_triggerId_fkey` FOREIGN KEY (`triggerId`) REFERENCES `PaybuttonTrigger`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `TriggerLog` ADD CONSTRAINT `TriggerLog_triggerId_fkey` FOREIGN KEY (`triggerId`) REFERENCES `PaybuttonTrigger`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
