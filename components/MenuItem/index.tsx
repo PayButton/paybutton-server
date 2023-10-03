b9f7941d90d425b81b0f9413aabd3f937c528b9c
chedieck <estevao@lobo.dev.br>
2023-10-03 16:55:14 -0300

    feat: change the color of admin button

--- components/MenuItem/index.tsx
+++ components/MenuItem/index.tsx
@@ -8,14 +8,18 @@ import Session from 'supertokens-web-js/recipe/session'
 export interface MenuItemProps {
   name: string
   image: string
+  isRestricted?: boolean
 }
 
-const MenuItem = ({ name, image }: MenuItemProps): FunctionComponent<MenuItemProps> => {
+const MenuItem = ({ name, image, isRestricted }: MenuItemProps): FunctionComponent<MenuItemProps> => {
   const { pathname } = useRouter()
   const href = '/' + name.toLowerCase()
   const isActive = pathname === href
 
-  const computedStyle = isActive || (pathname === '/button/[id]' && name === 'Buttons') ? `${style.li} ${style.active}` : style.li
+  let computedStyle = isActive || (pathname === '/button/[id]' && name === 'Buttons') ? `${style.li} ${style.active}` : style.li
+  if (isRestricted === true) {
+    computedStyle = `${computedStyle} ${style.restricted}`
+  }
 
   async function handleLogout (): Promise<void> {
     await Session.signOut()
@@ -30,7 +34,7 @@ const MenuItem = ({ name, image }: MenuItemProps): FunctionComponent<MenuItemPro
         </div>
         : <Link href={href} passHref>
             <div className={style.link}>
-            <div className={style.imagectn}><Image className={style.image} src={image} alt='PayButton' width={15} height={15} /></div>{name}
+              <div className={style.imagectn}><Image className={style.image} src={image} alt='PayButton' width={15} height={15} /></div>{name}
             </div>
       </Link>}
     </li>
