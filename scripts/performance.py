46ef5f4ce8d43f773038342ddd753af97e6f47dd
chedieck <estevao@lobo.dev.br>
2023-10-31 12:23:50 -0300

    feat: better speed & no csv file

--- scripts/performance.py
+++ scripts/performance.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import os
 import time
 
-SPEED_TICKS = 5
+SPEED_TICKS = 40
 
 def main():
     pass
@@ -64,10 +64,7 @@ def get_speed_command():
     ]
 
 def get_speed(insertion_list):
-    return sum(
-        (float(a[0]) - float(b[0]))/(a[1] - b[1]) for a, b in
-        zip(insertion_list[1:], insertion_list[:-1])
-    ) / (SPEED_TICKS - 1)
+    return (float(insertion_list[-1][0]) - float(insertion_list[0][0])) /  (insertion_list[-1][1] - insertion_list[0][1])
 
 def read_docker_stats():
     dev_cpu_list = []
@@ -77,35 +74,30 @@ def read_docker_stats():
     insertion_speed_list = []
 
     # Initialize CSV file
-    with open('docker_stats.csv', mode='w') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(['dev-cpu', 'db-cpu', 'dev-mem', 'db-mem'])
+    #with open('docker_stats.csv', mode='w') as csv_file:
+        #writer = csv.writer(csv_file)
+        #writer.writerow(['dev-cpu', 'db-cpu', 'dev-mem', 'db-mem'])
 
     # Launch the command as a subprocess that streams the data
     dev_process = subprocess.Popen(['docker', 'stats', 'paybutton-dev', '--format', 'json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     db_process = subprocess.Popen(['docker', 'stats', 'paybutton-db', '--format', 'json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     speed_command = get_speed_command()
 
-    last_speed_ticks = []
+    last_speed_ticks = [(0, 0)] * SPEED_TICKS
     while True:
         dev_output = dev_process.stdout.readline()
         db_output = db_process.stdout.readline()
         insertion_speed = subprocess.Popen(speed_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         insertion_time = time.time()
         speed_output = insertion_speed.stdout.readline()
-        print('oia o out', speed_output)
         if dev_output == '' and dev_process.poll() is not None:
             break
         if db_output == '' and db_process.poll() is not None:
             break
 
-        if len(last_speed_ticks) < 5:
-            last_speed_ticks.append((speed_output, insertion_time))
-            speed = 0
-        else:
-            last_speed_ticks.pop(0)
-            last_speed_ticks.append((speed_output, insertion_time))
-            speed = get_speed(last_speed_ticks)
+        last_speed_ticks.pop(0)
+        last_speed_ticks.append((speed_output, insertion_time))
+        speed = get_speed(last_speed_ticks)
 
         dev_stats = clean_docker_output(dev_output)
         db_stats = clean_docker_output(db_output)
