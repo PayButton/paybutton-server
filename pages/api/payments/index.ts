cf868ea16b5abc0cf6e37c5c191c0bc36d8dc222
chedieck <estevao@lobo.dev.br>
2023-11-14 17:20:04 -0300

    refactor: rename summarize -> cache

--- pages/api/payments/index.ts
+++ pages/api/payments/index.ts
@@ -159,9 +159,9 @@ export default async (req: any, res: any): Promise<void> => {
   if (req.method === 'GET') {
     await setSession(req, res)
     const userId = req.session.userId
-    const summarize = req.query.summarize as boolean
+    const cache = req.query.cache as boolean
     let resJSON: any
-    if (summarize) {
+    if (cache) {
       resJSON = await getUserDashboardData(userId)
     } else {
       resJSON = await getPaymentList(userId)
