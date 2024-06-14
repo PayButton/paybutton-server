486f4195232799c927edc046166fdfd1cd9880c8
lissavxo <lissavitoria12@gmail.com>
2024-06-14 15:16:34 -0300

    refactor: files util

--- utils/files.ts
+++ utils/files.ts
@@ -2,12 +2,15 @@ import { RESPONSE_MESSAGES } from "../constants/index";
 import { NextApiResponse } from "next";
 import { Transform } from "stream";
 
-export function resolveHeaders(headers:string[]): string {
-    return headers.join(',');
+export function valuesToCsvLine(values: string[], delimiter: string): string {
+    return values.join(delimiter);
+}
+interface DataObject {
+    [key: string]: any;
 }
 
-export function getDataFromValues(data:any, headers: string[]): any {
-    const result: any = {};
+export function getDataFromValues(data: DataObject, headers: string[]): Partial<DataObject> {
+    const result: Partial<DataObject> = {};
     headers.forEach(header => {
         result[header] = data[header];
     });
@@ -15,22 +18,22 @@ export function getDataFromValues(data:any, headers: string[]): any {
     return result;
 }
 
-export const getTransform = (headers:string[]) => new Transform({
+export const getTransform = (headers: string[], delimiter: string) => new Transform({
     objectMode: true,
     transform(chunk: any, encoding: BufferEncoding, callback: () => void) {
-        const csvLine = headers.map(header => chunk[header]).join(',') + '\n';
+        const csvLine = valuesToCsvLine(headers.map(header => chunk[header]), delimiter) + '\n';
         this.push(csvLine);
         callback();
     },
 });
 
-export function streamtoCSV(values: object[], headers: string[], res: NextApiResponse | any) {
-    const maxRecords = 10; 
-    const formattedHeaders = resolveHeaders(headers);
-    const transform = getTransform(headers)
+export function streamToCSV(values: object[], headers: string[], delimiter: string, res: NextApiResponse) {
+    const maxRecords = 10;
+    const csvLineHeaders = valuesToCsvLine(headers, delimiter);
+    const transform = getTransform(headers, delimiter);
 
     try {
-        res.write(formattedHeaders + '\n');
+        res.write(csvLineHeaders + '\n');
         values.slice(0, maxRecords).forEach((data: object) => {
             const formattedData = getDataFromValues(data, headers);
             transform.write(formattedData);
@@ -39,8 +42,7 @@ export function streamtoCSV(values: object[], headers: string[], res: NextApiRes
         transform.end();
         transform.pipe(res);
     } catch (error: any) {
-        console.log(error.message)
-        
-        throw new Error(RESPONSE_MESSAGES.COULD__NOT_DOWNLOAD_FILE_500.message)
+        console.error(error.message);
+        throw new Error(RESPONSE_MESSAGES.COULD_NOT_DOWNLOAD_FILE_500.message);
     }
 }
