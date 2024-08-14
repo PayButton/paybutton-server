c81fea7d6de43e75ac00b788fd9b26836f4380a6
chedieck <estevao@lobo.dev.br>
2024-08-14 08:59:40 -0300

    test: integration tests for tx detail

--- tests/integration-tests/blockchainClients.test.ts
+++ tests/integration-tests/blockchainClients.test.ts
@@ -1,12 +1,23 @@
-import { getLastBlockTimestamp } from 'services/blockchainService'
+import { getLastBlockTimestamp, getTransactionDetails } from 'services/blockchainService'
 
 describe('blockchain clients connect', () => {
   it('bitcoincash chronik connects', async () => {
-    const x = await getLastBlockTimestamp('bitcoincash')
-    expect(x).toBeGreaterThan(1723630000)
+    const t = await getLastBlockTimestamp('bitcoincash')
+    expect(t).toBeGreaterThan(1723630000)
   })
   it('eCash chronik connects', async () => {
-    const x = await getLastBlockTimestamp('ecash')
-    expect(x).toBeGreaterThan(1723630000)
+    const t = await getLastBlockTimestamp('ecash')
+    expect(t).toBeGreaterThan(1723630000)
+  })
+})
+
+describe('client fetches tx', () => {
+  it('bitcoincash chronik connects', async () => {
+    const tx = await getTransactionDetails('003688bbfe007a83dcbb59b329002306d94b7d1029f027399f2a30d6b3b7e5bb', 'bitcoincash')
+    expect(tx.block.height).toBe(858963)
+  })
+  it('eCash chronik connects', async () => {
+    const tx = await getTransactionDetails('ad5fd39867d70037251fb92a76b153a593a80e77457c16dc46aa12bd4aa70343', 'ecash')
+    expect(tx.block.height).toBe(857742)
   })
 })
