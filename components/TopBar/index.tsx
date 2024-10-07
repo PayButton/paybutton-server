ecb22f9c86508c38f35c871a913fe1ca01d9dbb1
John <jkuney17@gmail.com>
2024-10-07 16:27:56 -0500

    sidebar style tweaks

---
+++ components/TopBar/index.tsx
@@ -0,0 +1,10 @@
+// import Image from 'next/image';
+import style from './topbar.module.css'
+
+export default function TopBar (): JSX.Element {
+  return (
+    <div className={style.topbar_ctn}>
+
+    </div>
+  )
+}
