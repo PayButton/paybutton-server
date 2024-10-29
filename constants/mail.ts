f7d61de4f91ca6d7449e5222e8d6ad09d3c4d379
chedieck <estevao@lobo.dev.br>
2024-10-29 20:12:26 -0300

    feat: functional v1 email template

--- constants/mail.ts
+++ constants/mail.ts
@@ -4,8 +4,6 @@ import { XEC_TX_EXPLORER_URL, BCH_TX_EXPLORER_URL } from 'constants/index'
 import nodemailer from 'nodemailer'
 import { getAddressPrefix } from 'utils'
 import { OpReturnData } from 'utils/validators'
-import logoImageSource from 'assets/logo.png'
-import style from './mail.module.css'
 
 export const MAIL_TRANSPORTER = nodemailer.createTransport({
   host: config.smtpHost,
@@ -43,11 +41,7 @@ export const MAIL_HTML_REPLACER = (params: SendEmailParameters): string => {
   const messageText = params.opReturn.message === '' ? '' : `<li>Message: ${params.opReturn.message}</li>`
   const paymentIdText = params.opReturn.paymentId === '' ? '' : `<li>Payment ID: ${params.opReturn.paymentId}</li>`
   return `
-          <img src="assets/logo.png" alt="PayButton1">
-          a
-          <img src=${logoImageSource.src} alt="PayButton2">
-  <h1>Payment received on PayButton</h1>
-  <span> <b className=${style.blue} >${params.amount.toString()} ${params.currency}</b> received on PayButton <b>${params.buttonName}</b> </span>
+  <span> <b>${params.amount.toString()} ${params.currency}</b> received on PayButton <b>${params.buttonName}</b> </span>
   <ul>
     <li>Date and time: ${new Date(params.timestamp * 1000).toLocaleString()}</li>
     <li>Transaction ID: <a href=${explorerURL}${params.txId}>${params.txId}</a></li>
