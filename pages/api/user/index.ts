c99623a96aa47e4b47353d60365d0d760dbd3e61
chedieck <estevao@lobo.dev.br>
2024-10-30 20:45:30 -0300

    feat: api and service to update currency

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,14 +1,21 @@
 import { setSession } from 'utils/setSession'
 import * as userService from 'services/userService'
+import { parseUpdatePUTRequest } from 'utils/validators'
 
 export default async (
   req: any,
   res: any
 ): Promise<void> => {
   await setSession(req, res, true)
+  const session = req.session
   if (req.method === 'GET') {
-    const session = req.session
     const user = await userService.fetchUserProfileFromId(session.userId)
+    res.status(200).json(user)
+  }
+
+  if (req.method === 'PUT') {
+    const preferredCurrencyIdObject = parseUpdatePUTRequest(req.body)
+    const user = await userService.updatePreferredCurrency(session.userId, preferredCurrencyIdObject.currencyId)
     res.status(200).json(user)
   }
 }
