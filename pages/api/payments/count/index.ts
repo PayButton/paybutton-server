f171a1c690d02888d0194f995e2a2590765c7b3b
lissavxo <lissavitoria12@gmail.com>
2024-11-21 18:05:48 -0300

    feat: get payments count

---
+++ pages/api/payments/count/index.ts
@@ -0,0 +1,12 @@
+import { CacheGet } from 'redis/index'
+import { setSession } from 'utils/setSession'
+
+export default async (req: any, res: any): Promise<void> => {
+  if (req.method === 'GET') {
+    await setSession(req, res)
+    const userId = req.session.userId
+
+    const resJSON = await CacheGet.paymentListCount(userId)
+    res.status(200).json(resJSON)
+  }
+}
