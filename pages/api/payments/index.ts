5144ead949517faabf27d2ece71ef26f9eb16296
lissavxo <lissavitoria12@gmail.com>
2024-11-30 19:18:35 -0300

    feat: use getCachedPaymentsForUserWithPagination method

--- pages/api/payments/index.ts
+++ pages/api/payments/index.ts
@@ -5,7 +5,10 @@ export default async (req: any, res: any): Promise<void> => {
   if (req.method === 'GET') {
     await setSession(req, res)
     const userId = req.session.userId
-    const resJSON = await CacheGet.paymentList(userId)
+    const page = req.query.page as number
+    const pageSize = req.query.pageSize as number
+
+    const resJSON = await CacheGet.paymentListPaginated(userId, page, pageSize)
     res.status(200).json(resJSON)
   }
 }
