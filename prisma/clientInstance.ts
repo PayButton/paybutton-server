691fca37e6c23d763613baa595386f40f53bf3ea
Estevão <estevao@chedieck.com>
2024-12-11 08:33:14 -0300

    fix: instantiate only one prisma client

--- prisma/clientInstance.ts
+++ prisma/clientInstance.ts
@@ -1,21 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
-
 interface CustomNodeJsGlobal extends NodeJS.Global {
   prisma: PrismaClient
 }
 declare const global: CustomNodeJsGlobal
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (global.prisma === undefined) {
-    global.prisma = new PrismaClient()
-  }
-
-  prisma = global.prisma
+if (global.prisma === undefined) {
+  global.prisma = new PrismaClient()
 }
+
+const prisma = global.prisma
 
 export default prisma
 
