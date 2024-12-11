3dfaa97293d5bb9230b9b7a4aa0da12b00a7bfa2
Estevão <estevao@chedieck.com>
2024-12-10 21:17:54 -0300

    wip

--- prisma/clientInstance.ts
+++ prisma/clientInstance.ts
@@ -8,31 +8,9 @@ interface CustomNodeJsGlobal extends NodeJS.Global {
 declare const global: CustomNodeJsGlobal
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-  // Iterate over keys of PrismaClient
-  for (const modelKey of Object.keys(prisma) as Array<keyof PrismaClient>) {
-    const model = prisma[modelKey]
-
-    // Check if the model is an object (e.g., a Prisma model)
-    if (typeof model === 'object' && model !== null) {
-      for (const methodKey of Object.keys(model) as Array<keyof typeof model>) {
-        const originalMethod = model[methodKey]
-
-        // Check if the property is a function
-        if (typeof originalMethod === 'function') {
-          // Replace the original method with a wrapper
-          (model[methodKey] as unknown) = async (...args: unknown[]) => {
-            console.log(`Prisma Call: ${String(modelKey)}.${String(methodKey)}`)
-            console.log(`Args: ${JSON.stringify(args)}`)
-            console.trace('Stack Trace:')
-
-            // Call the original method
-            return (originalMethod as Function).apply(model, args)
-          }
-        }
-      }
-    }
-  }
+  prisma = new PrismaClient({
+    log: ['query', 'warn', 'info', 'error']
+  })
 } else {
   if (global.prisma === undefined) {
     global.prisma = new PrismaClient()
