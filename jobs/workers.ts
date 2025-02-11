a782b2dbb23b9d32348c78cbb086747862a2213a
Estevão <estevao@chedieck.com>
2025-02-10 22:23:24 -0300

    fix only no priecs

--- jobs/workers.ts
+++ jobs/workers.ts
@@ -89,13 +89,10 @@ export const connectAllTransactionsToPricesWorker = async (queueName: string): P
       console.log(`job ${job.id as string}: connecting prices to transactions...`)
       const noPricesTxs = await transactionService.fetchAllTransactionsWithNoPrices()
       console.log('got', noPricesTxs.length, 'txs with no prices')
-      const wrongNumberOfPricesTxs = await transactionService.fetchAllTransactionsWithIrregularPrices()
-      console.log('got', wrongNumberOfPricesTxs.length, 'txs with wrong n of prices')
       const txs = [
-        ...noPricesTxs,
-        ...wrongNumberOfPricesTxs
+        ...noPricesTxs
       ]
-      console.log(`found ${noPricesTxs.length} with no prices and ${wrongNumberOfPricesTxs.length} txs with wrong number of prices`)
+      console.log(`found ${noPricesTxs.length} with no prices and  txs with wrong number of prices`)
       void await transactionService.connectTransactionsListToPrices(txs)
     },
     {
