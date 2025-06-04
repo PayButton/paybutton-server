8a50bfd386bce788588c81b8759d743aaa307aa6
Estevão <estevao@chedieck.com>
2025-06-04 14:20:16 -0300

    feat: component, service & endpoint to get pro status

---
+++ components/Account/ProConfig.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+const ProConfig = (): JSX.Element => {
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    void (async () => {
+      const res = await fetch('/api/user/remainingProTime')
+      if (res.status === 200) {
+        const remainingMs: number | null = await res.json()
+        if (remainingMs === null) {
+          setText('You are not PRO.')
+        } else if (remainingMs <= 0) {
+          setText('Your PRO has expired.')
+        } else {
+          const futureDate = new Date(Date.now() + remainingMs)
+          setText(`You are PRO until ${futureDate.toLocaleDateString()}.`)
+        }
+      } else {
+        setText('Failed to fetch PRO status')
+      }
+    })()
+  }, [])
+
+  return <>{text}</>
+}
+
+export default ProConfig
