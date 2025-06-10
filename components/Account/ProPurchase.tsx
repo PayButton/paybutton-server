390c8e37f01300bba2da1ba7a96f7b22b5d5fe5e
Estevão <estevao@chedieck.com>
2025-06-10 08:01:18 -0300

    feat: make link button

--- components/Account/ProPurchase.tsx
+++ components/Account/ProPurchase.tsx
@@ -2,7 +2,9 @@ import Link from 'next/link'
 
 const ProPurchase = (): JSX.Element => {
   return <div className="WIP">
+  <button>
     <Link href='/pro'>Buy Pro</Link>
+  </button>
     </div>
 }
 
