a6943eb13485c8a656802c2e4e1eafa5f5d4f664
Estevão <estevao@chedieck.com>
2025-06-12 17:23:06 -0300

    feat: add hint

--- components/Account/ProDisplay.tsx
+++ components/Account/ProDisplay.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import ProPurchase from './ProPurchase'
 import style from './account.module.css'
+import stylep from '../../pages/account/account.module.css'
 
 const ProConfig = (): JSX.Element => {
   const [text, setText] = useState('')
   const [isPro, setIsPro] = useState<boolean | null>()
+  const [infoModal, setInfoModal] = useState(false)
 
   useEffect(() => {
     void (async () => {
@@ -31,8 +33,23 @@ const ProConfig = (): JSX.Element => {
   return <>
     <h3>PayButton Pro</h3>
     <div className={style.pro_ctn}>
-      <p>{text}</p>
+      <div className={stylep.account_row}>
+      <div className={stylep.label}>
+        {text}
+        <div
+          onClick={() => setInfoModal(!infoModal)}
+          className={stylep.whats_this_btn}
+        >
+          {infoModal ? 'Close' : 'What is this?'}
+        </div>
+      </div>
+      </div>
       {isPro === false && <ProPurchase/>}
+      {infoModal && (
+        <div className={stylep.public_key_info_ctn}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt risus libero, venenatis commodo nibh commodo eget. Aenean turpis tellus, consectetur vel consequat nec, vehicula quis odio. Sed vitae venenatis orci. Vestibulum a facilisis tellus. Nunc at hendrerit
+        </div>
+      )}
     </div>
   </>
 }
