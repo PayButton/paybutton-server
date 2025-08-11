baeee1cad7b48ea0f0ec305b5ed3f4174a0d2db3
Estevão <estevao@chedieck.com>
2025-08-11 15:28:13 -0300

    Revert "feat: add index to txs prices"
    This reverts commit 89ff3337da383bba52d2896a94519676d71209dc.

--- prisma/migrations/20250811182418_tx_prices_index/migration.sql
+++
@@ -1,3 +0,0 @@
--- RedefineIndex
-CREATE INDEX `idx_prices_on_transactions_transaction_id` ON `PricesOnTransactions`(`transactionId`);
-DROP INDEX `PricesOnTransactions_transactionId_fkey` ON `PricesOnTransactions`;
