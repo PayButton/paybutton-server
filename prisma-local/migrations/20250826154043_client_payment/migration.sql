5542edea0e93dfa88aa3325ca8eae1e781170cd7
lissavxo <lissavitoria12@gmail.com>
2025-08-26 17:40:35 -0300

    feat: add amount to client payment

--- prisma-local/migrations/20250826154043_client_payment/migration.sql
+++ prisma-local/migrations/20250826154043_client_payment/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE `ClientPayment` (
     `paymentId` VARCHAR(191) NOT NULL,
     `status` ENUM('PENDING', 'ADDED_TO_MEMPOOL', 'CONFIRMED') NOT NULL,
     `addressString` VARCHAR(191) NOT NULL,
+    `amount` DECIMAL(65, 30) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
