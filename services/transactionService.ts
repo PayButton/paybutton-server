bc6a25a3695f45fa0babfcc39c8f2a4883ab9bd5
lissavxo <lissavitoria12@gmail.com>
2025-08-27 13:31:29 -0300

    fix: check amount to update clientPayment

--- services/transactionService.ts
+++ services/transactionService.ts
@@ -1007,3 +1007,10 @@ export const updatePaymentStatus = async (paymentId: string, status: ClientPayme
     data: { status }
   })
 }
+
+export const getClientPayment = async (paymentId: string): Promise<Prisma.ClientPaymentGetPayload<{ include: { address: true } }>> => {
+  return await prisma.clientPayment.findUniqueOrThrow({
+    where: { paymentId },
+    include: { address: true }
+  })
+}
