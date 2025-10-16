aa8263c93f9a21e9e63caaa7adc7898c3e52064e
David Klakurka <klakurka@gmail.com>
2025-10-16 15:52:28 -0700

    Added security headers.

--- next.config.js
+++ next.config.js
@@ -29,6 +29,17 @@ const {
       typescript: {
         ignoreBuildErrors: true
       },
+      async headers() {
+        return [
+          {
+            source: '/:path*',
+            headers: [
+              { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+              { key: 'Content-Security-Policy', value: "frame-ancestors 'self'" },
+            ],
+          },
+        ]
+      },
       async rewrites() {
         return [
           {
