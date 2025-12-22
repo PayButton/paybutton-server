ce9cf2e3858f6f5cb7c7a661a561420d6b1279f8
David <Klakurka@users.noreply.github.com>
2025-12-22 15:09:35 -0800

    Updated loading spinner colour to use the standard blue (#1087)

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { DotLoader } from 'react-spinners'
 import style from './loading.module.css'
 
@@ -8,21 +8,10 @@ interface LoadingProps {
 }
 
 export default function Loading ({ size = 60, color }: LoadingProps): React.ReactElement {
-  const [accentColor, setAccentColor] = useState(color ?? '#0ac18e')
-
-  useEffect(() => {
-    if (color === undefined) {
-      const computedColor = getComputedStyle(document.documentElement).getPropertyValue('--accent-color').trim()
-      if (computedColor !== '') {
-        setAccentColor(computedColor)
-      }
-    }
-  }, [color])
-
   return (
     <div className={style.loading_container}>
       <DotLoader
-        color={accentColor}
+        color={color ?? 'var(--accent-color)'}
         size={size}
         speedMultiplier={2}
       />
