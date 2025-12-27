1025cd29789a6f5041079ac7c34e9f1303cc1a53
lissa <32442784+lissavxo@users.noreply.github.com>
2025-12-26 23:10:02 -0300

    fix: hidden buttons wallet edit form (#1096)
    * fix: hidden buttons wallet edit form
    
    * refactor: clean up

--- components/Wallet/EditWalletForm.tsx
+++ components/Wallet/EditWalletForm.tsx
@@ -87,60 +87,62 @@ export default function EditWalletForm ({ wallet, userAddresses, refreshWalletLi
               <h4>Edit {wallet.name}</h4>
               <div className={style_pb.form_ctn}>
                 <form onSubmit={(e) => { void handleSubmit(onSubmit)(e) }} method='post'>
-                  <label htmlFor='name'>Wallet Name</label>
-                  <input
-                    {...register('name')}
-                    type='text'
-                    id='name'
-                    name='name'
-                    placeholder={wallet.name}
-                    autoFocus
-                  />
-                  <h4>Addresses</h4>
-                  <div className={style.buttonlist_ctn}>
-                    {userAddresses.map((addr, index) => (
-                      <div className={style.input_field} key={`edit-addr-${addr.id}`}>
-                        <input
-                          type='checkbox'
-                          value={addr.id}
-                          id={`addressIdList.${index}`}
-                          defaultChecked={thisWalletAddressIdList.includes(addr.id)}
-                          onChange={ (e) => handleSelectedAddressesChange(e.target.checked, addr.id) }
-                        />
-                        <label htmlFor={`addressIdList.${index}`}>
-                          {addr.paybuttons.map((conn) => (
-                            <div className={style.buttonpill}>{conn.paybutton.name}</div>
-                          ))}
-                          <div className={style.addresslabel}>{addr.address}</div>
-                        </label>
-                      </div>
-                    ))}
-                  </div>
-                  <hr/>
-                  <div className={style.makedefault_ctn} key={`edit-wallet-${wallet.id}`}>
-                  {usedNetworks.some(network => network.ticker === 'xec') &&
-                    <div className={style.input_field}>
-                      <input
-                        {...register('isXECDefault')}
-                        defaultChecked={wallet.userProfile?.isXECDefault === true}
-                        type="checkbox"
-                        id='isXECDefault'
-                        disabled={wallet.userProfile?.isXECDefault === true}
-                      />
-                      <label htmlFor='xec-default' className={style.makedefault_margin}>Default XEC Wallet</label>
-                    </div>}
-                    {usedNetworks.some(network => network.ticker === 'bch') &&
-                    <div className={style.input_field}>
-                      <input
-                        {...register('isBCHDefault')}
-                        defaultChecked={wallet.userProfile?.isBCHDefault === true}
-                        type="checkbox"
-                        id='isBCHDefault'
-                        disabled={wallet.userProfile?.isBCHDefault === true}
-                      />
-                      <label htmlFor='bch-default' className={style.makedefault_margin}>Default BCH Wallet</label>
+                  <div style={{ overflowY: 'auto', maxHeight: 'calc(90vh - 150px)' }}>
+                    <label htmlFor='name'>Wallet Name</label>
+                    <input
+                      {...register('name')}
+                      type='text'
+                      id='name'
+                      name='name'
+                      placeholder={wallet.name}
+                      autoFocus
+                    />
+                    <h4>Addresses</h4>
+                    <div className={style.buttonlist_ctn}>
+                      {userAddresses.map((addr, index) => (
+                        <div className={style.input_field} key={`edit-addr-${addr.id}`}>
+                          <input
+                            type='checkbox'
+                            value={addr.id}
+                            id={`addressIdList.${index}`}
+                            defaultChecked={thisWalletAddressIdList.includes(addr.id)}
+                            onChange={ (e) => handleSelectedAddressesChange(e.target.checked, addr.id) }
+                          />
+                          <label htmlFor={`addressIdList.${index}`}>
+                            {addr.paybuttons.map((conn) => (
+                              <div key={conn.addressId} className={style.buttonpill}>{conn.paybutton.name}</div>
+                            ))}
+                            <div className={style.addresslabel}>{addr.address}</div>
+                          </label>
+                        </div>
+                      ))}
                     </div>
+                    <hr/>
+                    <div className={style.makedefault_ctn} key={`edit-wallet-${wallet.id}`}>
+                    {usedNetworks.some(network => network.ticker === 'xec') &&
+                      <div className={style.input_field}>
+                        <input
+                          {...register('isXECDefault')}
+                          defaultChecked={wallet.userProfile?.isXECDefault === true}
+                          type="checkbox"
+                          id='isXECDefault'
+                          disabled={wallet.userProfile?.isXECDefault === true}
+                        />
+                        <label htmlFor='isXECDefault' className={style.makedefault_margin}>Default XEC Wallet</label>
+                      </div>}
+                      {usedNetworks.some(network => network.ticker === 'bch') &&
+                      <div className={style.input_field}>
+                        <input
+                          {...register('isBCHDefault')}
+                          defaultChecked={wallet.userProfile?.isBCHDefault === true}
+                          type="checkbox"
+                          id='isBCHDefault'
+                          disabled={wallet.userProfile?.isBCHDefault === true}
+                        />
+                        <label htmlFor='isBCHDefault' className={style.makedefault_margin}>Default BCH Wallet</label>
+                      </div>
 }
+                    </div>
                   </div>
 
                   <div className={style_pb.btn_row2}>
