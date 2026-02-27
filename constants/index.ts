90df86f63ec42cd169100731c91a9fb0f43ec8ac
Fabien <fabcien@gmail.com>
2026-02-27 15:38:01 +0100

    Reduce concurrency
    For the chronik calls, and the db batch sizes. This avoids too long db transactions from impacting the performances and allow to start working faster on the chronik transactions.

--- constants/index.ts
+++ constants/index.ts
@@ -277,9 +277,9 @@ export const MEMPOOL_PROCESS_DELAY = 100
 // On chronik, the max allowed is 200
 export const CHRONIK_FETCH_N_TXS_PER_PAGE = 200
 
-export const INITIAL_ADDRESS_SYNC_FETCH_CONCURRENTLY = 128
-export const TX_EMIT_BATCH_SIZE = 2_000 // for our generator, not chronik
-export const DB_COMMIT_BATCH_SIZE = 2_000 // tamanho dos lotes para commit no DB
+export const INITIAL_ADDRESS_SYNC_FETCH_CONCURRENTLY = 16
+export const TX_EMIT_BATCH_SIZE = 200 // for our generator, not chronik
+export const DB_COMMIT_BATCH_SIZE = 200 // tamanho dos lotes para commit no DB
 
 export const TRIGGER_POST_CONCURRENCY = 100
 export const TRIGGER_EMAIL_CONCURRENCY = 100
